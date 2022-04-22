c0f65f11d9874dd7f572af19c6a08bf3205378d8
RustEnthusiast <RustEnthusiast@gmail.com>
2022-04-22 16:21:31 -0500

    Added `core` module.

---
+++ src/core.rs
@@ -0,0 +1,2 @@
+//! `nstd.core` is the central part of nstd. It makes no use of the Rust `std` crate, so it
+//! supports a wide range of systems, including embedded.
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@
 #![warn(missing_docs)]
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
+#[cfg(feature = "nstd_core")]
+#[cfg_attr(doc_cfg, doc(cfg(feature = "nstd_core")))]
+pub mod core;
