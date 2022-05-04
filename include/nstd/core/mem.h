7d9d6f66f6f3971798019d59ef21179be17e26b6
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-03 20:47:05 -0500

    Added `nstd_core_mem_copy_overlapping`.

--- include/nstd/core/mem.h
+++ include/nstd/core/mem.h
@@ -21,5 +21,23 @@ NSTDCPPSTART
 /// of a buffer.
 NSTDAPI void nstd_core_mem_copy(NSTDByte *dest, const NSTDByte *src, NSTDUSize num);
 
+/// Copies `num` bytes from `src` to `dest`. Unlike `nstd_core_mem_copy` this operation can be used
+/// when the two memory buffers overlap.
+///
+/// # Parameters:
+///
+/// - `NSTDByte *dest` - A pointer to the memory buffer to copy `src`'s bytes to.
+///
+/// - `const NSTDByte *src` - A pointer to the memory buffer to copy from.
+///
+/// - `NSTDUSize num` - The number of bytes to copy from `src` to `dest`.
+///
+/// # Safety
+///
+/// This function is highly unsafe as it does not know how large either of the memory buffers are,
+/// quickly leading to undefined behaviour if this function ends up reading or writing past the end
+/// of a buffer.
+NSTDAPI void nstd_core_mem_copy_overlapping(NSTDByte *dest, const NSTDByte *src, NSTDUSize num);
+
 NSTDCPPEND
 #endif
--- src/core/mem.rs
+++ src/core/mem.rs
@@ -25,3 +25,29 @@ pub unsafe extern "C" fn nstd_core_mem_copy(
 ) {
     core::ptr::copy_nonoverlapping(src, dest, num);
 }
+
+/// Copies `num` bytes from `src` to `dest`. Unlike `nstd_core_mem_copy` this operation can be used
+/// when the two memory buffers overlap.
+///
+/// # Parameters:
+///
+/// - `NSTDByte *dest` - A pointer to the memory buffer to copy `src`'s bytes to.
+///
+/// - `const NSTDByte *src` - A pointer to the memory buffer to copy from.
+///
+/// - `NSTDUSize num` - The number of bytes to copy from `src` to `dest`.
+///
+/// # Safety
+///
+/// This function is highly unsafe as it does not know how large either of the memory buffers are,
+/// quickly leading to undefined behaviour if this function ends up reading or writing past the end
+/// of a buffer.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_mem_copy_overlapping(
+    dest: *mut NSTDByte,
+    src: *const NSTDByte,
+    num: NSTDUSize,
+) {
+    core::ptr::copy(src, dest, num);
+}
