7a0137b76f0bee1b708459bd4601effbf1f5d07b
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-06 07:17:16 -0500

    Added `nstd_core_mem_compare`.

--- include/nstd/core/mem.h
+++ include/nstd/core/mem.h
@@ -4,6 +4,27 @@
 #include "../nstd.h"
 NSTDCPPSTART
 
+/// Compares two memory buffers of `num` bytes.
+///
+/// # Parameters:
+///
+/// - `const NSTDByte *buf1` - A pointer to the first memory buffer.
+///
+/// - `const NSTDByte *buf2` - A pointer to the second memory buffer.
+///
+/// - `NSTDUSize num` - The number of bytes to compare.
+///
+/// # Returns
+///
+/// `NSTDBool is_eq` - `NSTD_BOOL_TRUE` if the memory buffers carry the same data.
+///
+/// # Safety
+///
+/// This function is highly unsafe as it does not know how large either of the memory buffers
+/// actually are, which can lead to undefined behaviour if either of the buffers' length are less
+/// than `num`.
+NSTDAPI NSTDBool nstd_core_mem_compare(const NSTDByte *buf1, const NSTDByte *buf2, NSTDUSize num);
+
 /// Copies `num` bytes from `src` to `dest`.
 ///
 /// # Parameters:
--- src/core/mem.rs
+++ src/core/mem.rs
@@ -1,5 +1,41 @@
 //! Contains mostly unsafe functions for interacting with raw memory.
-use crate::core::def::{NSTDByte, NSTDUSize};
+use crate::core::def::{NSTDBool, NSTDByte, NSTDUSize};
+
+/// Compares two memory buffers of `num` bytes.
+///
+/// # Parameters:
+///
+/// - `const NSTDByte *buf1` - A pointer to the first memory buffer.
+///
+/// - `const NSTDByte *buf2` - A pointer to the second memory buffer.
+///
+/// - `NSTDUSize num` - The number of bytes to compare.
+///
+/// # Returns
+///
+/// `NSTDBool is_eq` - `NSTD_BOOL_TRUE` if the memory buffers carry the same data.
+///
+/// # Safety
+///
+/// This function is highly unsafe as it does not know how large either of the memory buffers
+/// actually are, which can lead to undefined behaviour if either of the buffers' length are less
+/// than `num`.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_mem_compare(
+    buf1: *const NSTDByte,
+    buf2: *const NSTDByte,
+    num: NSTDUSize,
+) -> NSTDBool {
+    // If the two pointers point to the same buffer, or `num` is 0, return true.
+    if buf1 == buf2 || num == 0 {
+        return NSTDBool::NSTD_BOOL_TRUE;
+    }
+    // Otherwise compare them manually.
+    let buf1 = core::slice::from_raw_parts(buf1, num);
+    let buf2 = core::slice::from_raw_parts(buf2, num);
+    (buf1 == buf2).into()
+}
 
 /// Copies `num` bytes from `src` to `dest`.
 ///
