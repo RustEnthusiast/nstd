c80289ad8d91ace0a3d2499aabc17411d6df3c6e
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-09 07:32:36 -0500

    Added `NSTDVec` struct.

--- include/nstd/vec.h
+++ include/nstd/vec.h
@@ -1,3 +1,15 @@
 #ifndef NSTD_VEC_H_INCLUDED
 #define NSTD_VEC_H_INCLUDED
+#include "core/def.h"
+#include "core/slice.h"
+#include "nstd.h"
+
+/// A dynamically sized contiguous sequence of values.
+typedef struct {
+    /// The underlying memory buffer.
+    NSTDSlice buffer;
+    /// The number of active elements in the vector.
+    NSTDUSize len;
+} NSTDVec;
+
 #endif
--- src/vec.rs
+++ src/vec.rs
@@ -1,1 +1,12 @@
 //! A dynamically sized contiguous sequence of values.
+use crate::core::{def::NSTDUSize, slice::NSTDSlice};
+
+/// A dynamically sized contiguous sequence of values.
+#[repr(C)]
+#[derive(Debug, Hash)]
+pub struct NSTDVec {
+    /// The underlying memory buffer.
+    pub buffer: NSTDSlice,
+    /// The number of active elements in the vector.
+    pub len: NSTDUSize,
+}
