7c96b6244ce0e7f1da48355c47cd205f1d7bcb72
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-10 17:20:04 -0500

    Added `nstd_vec_[new|free]`.

--- include/nstd/vec.h
+++ include/nstd/vec.h
@@ -3,6 +3,7 @@
 #include "core/def.h"
 #include "core/slice.h"
 #include "nstd.h"
+NSTDCPPSTART
 
 /// A dynamically sized contiguous sequence of values.
 typedef struct {
@@ -12,4 +13,23 @@ typedef struct {
     NSTDUSize len;
 } NSTDVec;
 
+/// Creates a new vector without allocating any resources.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize element_size` - The size in bytes of each value in the vector.
+///
+/// # Returns
+///
+/// `NSTDVec vec` - The new vector.
+NSTDAPI NSTDVec nstd_vec_new(NSTDUSize element_size);
+
+/// Frees an instance of `NSTDVec`.
+///
+/// # Parameters:
+///
+/// - `NSTDVec *vec` - The vector to free.
+NSTDAPI void nstd_vec_free(NSTDVec *vec);
+
+NSTDCPPEND
 #endif
--- src/vec.rs
+++ src/vec.rs
@@ -1,5 +1,12 @@
 //! A dynamically sized contiguous sequence of values.
-use crate::core::{def::NSTDUSize, slice::NSTDSlice};
+use crate::{
+    alloc::nstd_alloc_deallocate,
+    core::{
+        def::NSTDUSize,
+        slice::{nstd_core_slice_new, NSTDSlice},
+        NSTD_CORE_NULL,
+    },
+};
 
 /// A dynamically sized contiguous sequence of values.
 #[repr(C)]
@@ -9,4 +16,38 @@ pub struct NSTDVec {
     pub buffer: NSTDSlice,
     /// The number of active elements in the vector.
     pub len: NSTDUSize,
+}
+
+/// Creates a new vector without allocating any resources.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize element_size` - The size in bytes of each value in the vector.
+///
+/// # Returns
+///
+/// `NSTDVec vec` - The new vector.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_vec_new(element_size: NSTDUSize) -> NSTDVec {
+    NSTDVec {
+        buffer: nstd_core_slice_new(NSTD_CORE_NULL, element_size, 0),
+        len: 0,
+    }
+}
+
+/// Frees an instance of `NSTDVec`.
+///
+/// # Parameters:
+///
+/// - `NSTDVec *vec` - The vector to free.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_vec_free(vec: &mut NSTDVec) {
+    if !vec.buffer.ptr.raw.is_null() {
+        let buffer_len = vec.buffer.byte_len();
+        unsafe { nstd_alloc_deallocate(&mut vec.buffer.ptr.raw, buffer_len) };
+        vec.buffer.len = 0;
+        vec.len = 0;
+    }
 }
