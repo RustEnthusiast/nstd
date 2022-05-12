8ccc2fcd6f043c72a4570074ec38322583e51ce1
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-12 16:08:22 -0500

    Run `cargo check` instead of `cargo build`.

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import os
 FEATURES = ("nstd_alloc", "nstd_core", "nstd_os_alloc")
 
 for feature in FEATURES:
-    cmd = f"cargo build --quiet --no-default-features --features \"std {feature}\""
+    cmd = f"cargo check --quiet --no-default-features --features \"std {feature}\""
     print(f"Running {cmd}...")
     os.system(cmd)
