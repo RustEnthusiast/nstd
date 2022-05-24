71431336cef7354e5032c02007d9d8d4a248a2ff
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-24 11:39:10 -0500

    Added `NSTDString` struct.

--- include/nstd/string.h
+++ include/nstd/string.h
@@ -1,4 +1,12 @@
 #ifndef NSTD_STRING_H_INCLUDED
 #define NSTD_STRING_H_INCLUDED
 #include "nstd.h"
+#include "vec.h"
+
+/// Dynamically sized UTF-8 encoded byte string.
+typedef struct {
+    /// The underlying UTF-8 encoded byte buffer.
+    NSTDVec bytes;
+} NSTDString;
+
 #endif
--- src/string.rs
+++ src/string.rs
@@ -1,1 +1,10 @@
 //! Dynamically sized UTF-8 encoded byte string.
+use crate::vec::NSTDVec;
+
+/// Dynamically sized UTF-8 encoded byte string.
+#[repr(C)]
+#[derive(Clone, Debug, Hash)]
+pub struct NSTDString {
+    /// The underlying UTF-8 encoded byte buffer.
+    pub bytes: NSTDVec,
+}
