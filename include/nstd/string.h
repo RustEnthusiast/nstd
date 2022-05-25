d5d5515ee5ef1f2de70fad18a80b06bb2fecc951
RustEnthusiast <RustEnthusiast@gmail.com>
2022-05-25 10:34:24 -0500

    Added `nstd_string_new_with_cap`.

--- include/nstd/string.h
+++ include/nstd/string.h
@@ -1,5 +1,6 @@
 #ifndef NSTD_STRING_H_INCLUDED
 #define NSTD_STRING_H_INCLUDED
+#include "core/def.h"
 #include "nstd.h"
 #include "vec.h"
 NSTDCPPSTART
@@ -16,6 +17,21 @@ typedef struct {
 ///
 /// `NSTDString string` - The new string.
 NSTDAPI NSTDString nstd_string_new();
+
+/// Creates a new string initialized with the given capacity.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize cap` - The number of bytes to preallocate.
+///
+/// # Returns
+///
+/// `NSTDString string` - The new string.
+///
+/// # Panics
+///
+/// This function will panic if `cap` is zero.
+NSTDAPI NSTDString nstd_string_new_with_cap(NSTDUSize cap);
 
 /// Frees an instance of `NSTDString`.
 ///
--- src/string.rs
+++ src/string.rs
@@ -1,5 +1,8 @@
 //! Dynamically sized UTF-8 encoded byte string.
-use crate::vec::{nstd_vec_free, nstd_vec_new, NSTDVec};
+use crate::{
+    core::def::NSTDUSize,
+    vec::{nstd_vec_free, nstd_vec_new, nstd_vec_new_with_cap, NSTDVec},
+};
 
 /// Dynamically sized UTF-8 encoded byte string.
 #[repr(C)]
@@ -19,6 +22,27 @@ pub struct NSTDString {
 pub extern "C" fn nstd_string_new() -> NSTDString {
     NSTDString {
         bytes: nstd_vec_new(1),
+    }
+}
+
+/// Creates a new string initialized with the given capacity.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize cap` - The number of bytes to preallocate.
+///
+/// # Returns
+///
+/// `NSTDString string` - The new string.
+///
+/// # Panics
+///
+/// This function will panic if `cap` is zero.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_string_new_with_cap(cap: NSTDUSize) -> NSTDString {
+    NSTDString {
+        bytes: nstd_vec_new_with_cap(1, cap),
     }
 }
 
