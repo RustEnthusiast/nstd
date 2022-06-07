df4d01afb2899bb7f14cf9d3246ffd139ca22a36
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-07 13:00:25 -0500

    Added `NSTDHeapPtr` struct.

--- include/nstd/heap_ptr.h
+++ include/nstd/heap_ptr.h
@@ -1,4 +1,12 @@
 #ifndef NSTD_HEAP_PTR_H_INCLUDED
 #define NSTD_HEAP_PTR_H_INCLUDED
+#include "core/ptr.h"
 #include "nstd.h"
+
+/// A pointer type for single value heap allocation.
+typedef struct {
+    /// A pointer to the value on the heap.
+    NSTDPtr ptr;
+} NSTDHeapPtr;
+
 #endif
--- src/heap_ptr.rs
+++ src/heap_ptr.rs
@@ -1,1 +1,10 @@
 //! A pointer type for single value heap allocation.
+use crate::core::ptr::NSTDPtr;
+
+/// A pointer type for single value heap allocation.
+#[repr(C)]
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct NSTDHeapPtr {
+    /// A pointer to the value on the heap.
+    ptr: NSTDPtr,
+}
