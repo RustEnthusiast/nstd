34db19c5728d988e128dff51085ff24dc20a4ff6
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-08 12:46:28 -0500

    Added `nstd_heap_ptr_[new|free]`.

--- include/nstd/heap_ptr.h
+++ include/nstd/heap_ptr.h
@@ -2,6 +2,7 @@
 #define NSTD_HEAP_PTR_H_INCLUDED
 #include "core/ptr.h"
 #include "nstd.h"
+NSTDCPPSTART
 
 /// A pointer type for single value heap allocation.
 typedef struct {
@@ -9,4 +10,27 @@ typedef struct {
     NSTDPtr ptr;
 } NSTDHeapPtr;
 
+/// Creates a new zero-initialized heap allocated object.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize element_size` - The size (in bytes) of the heap object.
+///
+/// # Returns
+///
+/// `NSTDHeapPtr hptr` - The new heap allocated object.
+///
+/// # Panics
+///
+/// This function will panic if either `element_size` is zero, or allocation fails.
+NSTDAPI NSTDHeapPtr nstd_heap_ptr_new(NSTDUSize element_size);
+
+/// Frees an instance of `NSTDHeapPtr`.
+///
+/// # Parameters:
+///
+/// - `NSTDHeapPtr *hptr` - A pointer to the heap object.
+NSTDAPI void nstd_heap_ptr_free(NSTDHeapPtr *hptr);
+
+NSTDCPPEND
 #endif
--- src/heap_ptr.rs
+++ src/heap_ptr.rs
@@ -1,5 +1,9 @@
 //! A pointer type for single value heap allocation.
-use crate::core::ptr::NSTDPtr;
+use crate::{
+    alloc::{nstd_alloc_allocate_zeroed, nstd_alloc_deallocate},
+    core::ptr::{nstd_core_ptr_new, NSTDPtr},
+    NSTDUSize,
+};
 
 /// A pointer type for single value heap allocation.
 #[repr(C)]
@@ -7,4 +11,41 @@ use crate::core::ptr::NSTDPtr;
 pub struct NSTDHeapPtr {
     /// A pointer to the value on the heap.
     ptr: NSTDPtr,
+}
+
+/// Creates a new zero-initialized heap allocated object.
+///
+/// # Parameters:
+///
+/// - `NSTDUSize element_size` - The size (in bytes) of the heap object.
+///
+/// # Returns
+///
+/// `NSTDHeapPtr hptr` - The new heap allocated object.
+///
+/// # Panics
+///
+/// This function will panic if either `element_size` is zero, or allocation fails.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_heap_ptr_new(element_size: NSTDUSize) -> NSTDHeapPtr {
+    assert!(element_size != 0);
+    let mem = unsafe { nstd_alloc_allocate_zeroed(element_size) };
+    assert!(!mem.is_null());
+    NSTDHeapPtr {
+        ptr: nstd_core_ptr_new(mem, element_size),
+    }
+}
+
+/// Frees an instance of `NSTDHeapPtr`.
+///
+/// # Parameters:
+///
+/// - `NSTDHeapPtr *hptr` - A pointer to the heap object.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_heap_ptr_free(hptr: &mut NSTDHeapPtr) {
+    if !hptr.ptr.raw.is_null() {
+        unsafe { nstd_alloc_deallocate(&mut hptr.ptr.raw, hptr.ptr.size) };
+    }
 }
