a52172f825fef3c33c19bc4abe952c9815e57337
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-13 04:57:56 -0500

    Added `NSTDCStr[Const]`.

--- include/nstd/core/cstr.h
+++ include/nstd/core/cstr.h
@@ -4,6 +4,107 @@
 #include "def.h"
 NSTDCPPSTART
 
+/// A mutable slice of a C string.
+typedef struct {
+    /// A pointer to the first character in the C string.
+    NSTDChar *ptr;
+    /// The length of the C string, excluding the null byte.
+    NSTDUSize len;
+} NSTDCStr;
+
+/// Creates a new C string slice from a raw pointer and a size.
+///
+/// # Parameters:
+///
+/// - `NSTDChar *raw` - A pointer to the first character to be in the C string slice.
+///
+/// - `NSTDUSize len` - The length of the C string, excluding the null byte.
+///
+/// # Returns
+///
+/// `NSTDCStr cstr` - The new C string slice, referencing `raw`'s data.
+///
+/// # Safety
+///
+/// `raw`'s data must remain valid while the returned C string slice is in use.
+NSTDAPI NSTDCStr nstd_core_cstr_new(NSTDChar *raw, NSTDUSize len);
+
+/// Return a pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `NSTDCStr *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+NSTDAPI NSTDChar *nstd_core_cstr_get(NSTDCStr *cstr, NSTDUSize pos);
+
+/// Return an immutable pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `const NSTDCStr *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `const NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+NSTDAPI const NSTDChar *nstd_core_cstr_get_const(const NSTDCStr *cstr, NSTDUSize pos);
+
+/// An immutable slice of a C string.
+typedef struct {
+    /// A pointer to the first character in the C string.
+    const NSTDChar *ptr;
+    /// The length of the C string, excluding the null byte.
+    NSTDUSize len;
+} NSTDCStrConst;
+
+/// Creates a new C string slice from a raw pointer and a size.
+///
+/// # Parameters:
+///
+/// - `const NSTDChar *raw` - A pointer to the first character to be in the C string slice.
+///
+/// - `NSTDUSize len` - The length of the C string, excluding the null byte.
+///
+/// # Returns
+///
+/// `NSTDCStrConst cstr` - The new C string slice, referencing `raw`'s data.
+///
+/// # Safety
+///
+/// `raw`'s data must remain valid while the returned C string slice is in use.
+NSTDAPI NSTDCStrConst nstd_core_cstr_const_new(const NSTDChar *raw, NSTDUSize len);
+
+/// Return a pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `const NSTDCStrConst *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `const NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+NSTDAPI const NSTDChar *nstd_core_cstr_const_get(const NSTDCStrConst *cstr, NSTDUSize pos);
+
 /// Gets the length of a null terminated C string, excluding the null byte.
 ///
 /// # Parameters:
--- src/core/cstr.rs
+++ src/core/cstr.rs
@@ -6,6 +6,146 @@
 //! pointers.
 use crate::{core::def::NSTDChar, NSTDBool, NSTDUSize, NSTD_FALSE, NSTD_TRUE};
 
+/// A mutable slice of a C string.
+#[repr(C)]
+#[derive(Debug, Hash)]
+pub struct NSTDCStr {
+    /// A pointer to the first character in the C string.
+    pub ptr: *mut NSTDChar,
+    /// The length of the C string, excluding the null byte.
+    pub len: NSTDUSize,
+}
+
+/// Creates a new C string slice from a raw pointer and a size.
+///
+/// # Parameters:
+///
+/// - `NSTDChar *raw` - A pointer to the first character to be in the C string slice.
+///
+/// - `NSTDUSize len` - The length of the C string, excluding the null byte.
+///
+/// # Returns
+///
+/// `NSTDCStr cstr` - The new C string slice, referencing `raw`'s data.
+///
+/// # Safety
+///
+/// `raw`'s data must remain valid while the returned C string slice is in use.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_cstr_new(raw: *mut NSTDChar, len: NSTDUSize) -> NSTDCStr {
+    NSTDCStr { ptr: raw, len }
+}
+
+/// Return a pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `NSTDCStr *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_cstr_get(cstr: &mut NSTDCStr, pos: NSTDUSize) -> *mut NSTDChar {
+    nstd_core_cstr_get_const(cstr, pos) as *mut NSTDChar
+}
+
+/// Return an immutable pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `const NSTDCStr *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `const NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_cstr_get_const(
+    cstr: &NSTDCStr,
+    pos: NSTDUSize,
+) -> *const NSTDChar {
+    match pos < cstr.len {
+        true => cstr.ptr.add(pos),
+        false => core::ptr::null(),
+    }
+}
+
+/// An immutable slice of a C string.
+#[repr(C)]
+#[derive(Debug, Hash)]
+pub struct NSTDCStrConst {
+    /// A pointer to the first character in the C string.
+    pub ptr: *const NSTDChar,
+    /// The length of the C string, excluding the null byte.
+    pub len: NSTDUSize,
+}
+
+/// Creates a new C string slice from a raw pointer and a size.
+///
+/// # Parameters:
+///
+/// - `const NSTDChar *raw` - A pointer to the first character to be in the C string slice.
+///
+/// - `NSTDUSize len` - The length of the C string, excluding the null byte.
+///
+/// # Returns
+///
+/// `NSTDCStrConst cstr` - The new C string slice, referencing `raw`'s data.
+///
+/// # Safety
+///
+/// `raw`'s data must remain valid while the returned C string slice is in use.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_cstr_const_new(
+    raw: *const NSTDChar,
+    len: NSTDUSize,
+) -> NSTDCStrConst {
+    NSTDCStrConst { ptr: raw, len }
+}
+
+/// Return a pointer the character at `pos` in `cstr`.
+///
+/// # Parameters:
+///
+/// - `const NSTDCStrConst *cstr` - The C string.
+///
+/// - `NSTDUSize pos` - The position of the character to get.
+///
+/// # Returns
+///
+/// `const NSTDChar *chr` - A pointer to the character at `pos`, or null on error.
+///
+/// # Safety
+///
+/// `cstr`'s data must remain valid while the returned pointer is in use.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_core_cstr_const_get(
+    cstr: &NSTDCStrConst,
+    pos: NSTDUSize,
+) -> *const NSTDChar {
+    match pos < cstr.len {
+        true => cstr.ptr.add(pos),
+        false => core::ptr::null(),
+    }
+}
+
 /// Gets the length of a null terminated C string, excluding the null byte.
 ///
 /// # Parameters:
