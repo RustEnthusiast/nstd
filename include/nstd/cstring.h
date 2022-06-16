97803db4a20e09b03ca9c951148270c1c48fee52
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-16 05:28:21 -0500

    Added `NSTDCString`.

--- include/nstd/cstring.h
+++ include/nstd/cstring.h
@@ -1,4 +1,12 @@
 #ifndef NSTD_CSTRING_H_INCLUDED
 #define NSTD_CSTRING_H_INCLUDED
 #include "nstd.h"
+#include "vec.h"
+
+/// A dynamically sized, null terminated, C string.
+typedef struct {
+    /// The underlying vector of `NSTDChar`s.
+    NSTDVec bytes;
+} NSTDCString;
+
 #endif
--- src/cstring.rs
+++ src/cstring.rs
@@ -1,1 +1,10 @@
 //! A dynamically sized, null terminated, C string.
+use crate::vec::NSTDVec;
+
+/// A dynamically sized, null terminated, C string.
+#[repr(C)]
+#[derive(Debug, Hash)]
+pub struct NSTDCString {
+    /// The underlying vector of `NSTDChar`s.
+    pub bytes: NSTDVec,
+}
