2d20898da9844afa84c4c6e4e9a0900d8e6daf91
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-17 14:29:41 -0500

    Added `nstd_cstring_[new|free]`.

--- include/nstd/cstring.h
+++ include/nstd/cstring.h
@@ -2,6 +2,7 @@
 #define NSTD_CSTRING_H_INCLUDED
 #include "nstd.h"
 #include "vec.h"
+NSTDCPPSTART
 
 /// A dynamically sized, null terminated, C string.
 typedef struct {
@@ -9,4 +10,19 @@ typedef struct {
     NSTDVec bytes;
 } NSTDCString;
 
+/// Creates a new empty `NSTDCString`.
+///
+/// # Returns
+///
+/// `NSTDCString cstring` - The new C string.
+NSTDAPI NSTDCString nstd_cstring_new();
+
+/// Frees an instance of `NSTDCString`.
+///
+/// # Parameters:
+///
+/// - `NSTDCString *cstring` - The C string to free.
+NSTDAPI void nstd_cstring_free(NSTDCString *cstring);
+
+NSTDCPPEND
 #endif
--- src/cstring.rs
+++ src/cstring.rs
@@ -1,5 +1,5 @@
 //! A dynamically sized, null terminated, C string.
-use crate::vec::NSTDVec;
+use crate::vec::{nstd_vec_free, nstd_vec_new, NSTDVec};
 
 /// A dynamically sized, null terminated, C string.
 #[repr(C)]
@@ -7,4 +7,28 @@ use crate::vec::NSTDVec;
 pub struct NSTDCString {
     /// The underlying vector of `NSTDChar`s.
     pub bytes: NSTDVec,
+}
+
+/// Creates a new empty `NSTDCString`.
+///
+/// # Returns
+///
+/// `NSTDCString cstring` - The new C string.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_cstring_new() -> NSTDCString {
+    NSTDCString {
+        bytes: nstd_vec_new(1),
+    }
+}
+
+/// Frees an instance of `NSTDCString`.
+///
+/// # Parameters:
+///
+/// - `NSTDCString *cstring` - The C string to free.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_cstring_free(cstring: &mut NSTDCString) {
+    nstd_vec_free(&mut cstring.bytes);
 }
