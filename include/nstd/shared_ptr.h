20a3029243d1a940f56497ea712b79d0e4e0127d
RustEnthusiast <RustEnthusiast@gmail.com>
2022-06-22 02:06:55 -0500

    Added `NSTDSharedPtr`.

--- include/nstd/shared_ptr.h
+++ include/nstd/shared_ptr.h
@@ -1,4 +1,12 @@
 #ifndef NSTD_SHARED_PTR_H_INCLUDED
 #define NSTD_SHARED_PTR_H_INCLUDED
+#include "heap_ptr.h"
 #include "nstd.h"
+
+/// A reference counting smart pointer.
+typedef struct {
+    /// A heap pointer to private data about the shared object.
+    NSTDHeapPtr ptr;
+} NSTDSharedPtr;
+
 #endif
--- src/shared_ptr.rs
+++ src/shared_ptr.rs
@@ -1,1 +1,10 @@
 //! A reference counting smart pointer.
+use crate::heap_ptr::NSTDHeapPtr;
+
+/// A reference counting smart pointer.
+#[repr(C)]
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct NSTDSharedPtr {
+    /// A heap pointer to private data about the shared object.
+    pub ptr: NSTDHeapPtr,
+}
