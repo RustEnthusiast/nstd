40327fb5b8dc7efe6b2b2e42564d1898b8418906
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-01 17:15:00 -0500

    Updated test.py.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 FEATURES = ("nstd_alloc", "nstd_core", "nstd_cstring", "nstd_heap_ptr",
-            "nstd_os_alloc", "nstd_shared_ptr", "nstd_string", "nstd_vec")
+            "nstd_os_windows_alloc", "nstd_shared_ptr", "nstd_string", "nstd_vec")
 
 for feature in FEATURES:
     cmd = f"cargo check --quiet --no-default-features --features \"std {feature}\""
