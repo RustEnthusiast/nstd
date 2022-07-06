85eead8c870d532de689a9f7c7fcfc4363f30901
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-06 15:06:11 -0500

    Update `test.py`.

--- test.py
+++ test.py
@@ -3,7 +3,14 @@ import os
 FEATURES = ("nstd_alloc", "nstd_core", "nstd_cstring", "nstd_heap_ptr",
             "nstd_os_windows_alloc", "nstd_shared_ptr", "nstd_string", "nstd_vec")
 
-for feature in FEATURES:
-    cmd = f"cargo check --quiet --no-default-features --features \"std {feature}\""
-    print(f"Running {cmd}...")
-    os.system(cmd)
+TARGETS = ("x86_64-pc-windows-msvc", "x86_64-apple-darwin",
+           "x86_64-unknown-linux-gnu", "x86_64-apple-ios", "x86_64-linux-android")
+
+if __name__ == "__main__":
+    for target in TARGETS:
+        for feature in FEATURES:
+            feature_tag = f"--features \"std {feature}\""
+            target_tag = f"--target {target}"
+            cmd = f"cargo check --quiet --no-default-features {feature_tag} {target_tag}"
+            print(f"Running {cmd}...")
+            os.system(cmd)
