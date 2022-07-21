29acd673df4f893fdece1e859baaf40bcc517099
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-21 01:24:22 -0500

    Added `nstd_io_print`.

--- include/nstd/io.h
+++ include/nstd/io.h
@@ -1,4 +1,30 @@
 #ifndef NSTD_IO_H_INCLUDED
 #define NSTD_IO_H_INCLUDED
+#include "core/def.h"
 #include "nstd.h"
+NSTDCPPSTART
+
+/// Writes a raw null-terminated C string to stdout.
+///
+/// # Parameters:
+///
+/// - `const NSTDChar *cstr` - The raw null-terminated C string.
+///
+/// # Returns
+///
+/// `NSTDErrorCode errc` - Nonzero on error.
+///
+/// # Possible errors
+///
+/// - `1` - Writing `cstr`'s bytes to stdout failed.
+///
+/// - `2` - Flushing stdout failed.
+///
+/// # Safety
+///
+/// The provided C string must be null terminated, else this function can cause garbage bytes to be
+/// written to stdout.
+NSTDAPI NSTDErrorCode nstd_io_print(const NSTDChar *cstr);
+
+NSTDCPPEND
 #endif
--- src/io.rs
+++ src/io.rs
@@ -1,1 +1,39 @@
 //! Provides functionality for interacting with the standard I/O streams.
+use crate::core::{
+    cstr::raw::nstd_core_cstr_raw_len,
+    def::{NSTDChar, NSTDErrorCode},
+};
+use std::io::Write;
+
+/// Writes a raw null-terminated C string to stdout.
+///
+/// # Parameters:
+///
+/// - `const NSTDChar *cstr` - The raw null-terminated C string.
+///
+/// # Returns
+///
+/// `NSTDErrorCode errc` - Nonzero on error.
+///
+/// # Possible errors
+///
+/// - `1` - Writing `cstr`'s bytes to stdout failed.
+///
+/// - `2` - Flushing stdout failed.
+///
+/// # Safety
+///
+/// The provided C string must be null terminated, else this function can cause garbage bytes to be
+/// written to stdout.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_io_print(cstr: *const NSTDChar) -> NSTDErrorCode {
+    let len = nstd_core_cstr_raw_len(cstr);
+    let bytes = std::slice::from_raw_parts(cstr.cast(), len);
+    let mut stdout = std::io::stdout();
+    if let Err(_) = stdout.write_all(bytes) {
+        return 1;
+    } else if let Err(_) = stdout.flush() {
+        return 2;
+    }
+    0
+}
