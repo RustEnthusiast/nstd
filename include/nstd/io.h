d0a9b4c30e09f272cb10491dca0116aa24562f5a
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-26 13:02:27 -0500

    Added `nstd_io_read`.

--- include/nstd/io.h
+++ include/nstd/io.h
@@ -3,6 +3,7 @@
 #include "core/cstr/cstr.h"
 #include "core/def.h"
 #include "nstd.h"
+#include "string.h"
 NSTDCPPSTART
 
 /// Writes a C string slice to stdout.
@@ -26,6 +27,13 @@ NSTDCPPSTART
 /// The provided C string slice's data must be valid, else this function can cause garbage bytes to
 /// be written to stdout.
 NSTDAPI NSTDErrorCode nstd_io_print(const NSTDCStrConst *cstr);
+
+/// Reads a line of UTF-8 input from stdin and returns it, discarding the newline.
+///
+/// # Returns
+///
+/// `NSTDString input` - The input from stdin, or an empty string on error.
+NSTDAPI NSTDString nstd_io_read();
 
 NSTDCPPEND
 #endif
--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,12 @@
 //! Provides functionality for interacting with the standard I/O streams.
-use crate::core::{
-    cstr::{nstd_core_cstr_const_as_ptr, nstd_core_cstr_const_len, NSTDCStrConst},
-    def::NSTDErrorCode,
+use crate::{
+    core::{
+        cstr::{nstd_core_cstr_const_as_ptr, nstd_core_cstr_const_len, NSTDCStrConst},
+        def::NSTDErrorCode,
+    },
+    string::{nstd_string_new, NSTDString},
 };
-use std::io::Write;
+use std::io::{prelude::*, BufReader};
 
 /// Writes a C string slice to stdout.
 ///
@@ -37,4 +40,19 @@ pub unsafe extern "C" fn nstd_io_print(cstr: &NSTDCStrConst) -> NSTDErrorCode {
         return 2;
     }
     0
+}
+
+/// Reads a line of UTF-8 input from stdin and returns it, discarding the newline.
+///
+/// # Returns
+///
+/// `NSTDString input` - The input from stdin, or an empty string on error.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_io_read() -> NSTDString {
+    let mut buffer = String::new();
+    if let Err(_) = BufReader::new(std::io::stdin()).read_line(&mut buffer) {
+        return nstd_string_new();
+    }
+    buffer.pop();
+    NSTDString::from(buffer.as_str())
 }
