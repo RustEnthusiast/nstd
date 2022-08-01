38ccce0c92b99d46fdac5696de53d73658e75b78
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-31 22:14:30 -0500

    Added `NSTDIOError`.

--- include/nstd/io/io.h
+++ include/nstd/io/io.h
@@ -6,6 +6,52 @@
 #include "../string.h"
 NSTDCPPSTART
 
+/// An error type for I/O operations.
+typedef enum {
+    /// No error ocurred.
+    NSTD_IO_ERROR_NONE,
+    /// An unknown/other error ocurred.
+    NSTD_IO_ERROR_UNKNOWN,
+    /// An entity, such as a file, was not found.
+    NSTD_IO_ERROR_NOT_FOUND,
+    /// Permission was denied.
+    NSTD_IO_ERROR_PERMISSION_DENIED,
+    /// The connection was refused by a remote server.
+    NSTD_IO_ERROR_CONNECTION_REFUSED,
+    /// The connection was reset by a remote server.
+    NSTD_IO_ERROR_CONNECTION_RESET,
+    /// The connection was terminated by a remote server.
+    NSTD_IO_ERROR_CONNECTION_TERMINATED,
+    /// There is no connection.
+    NSTD_IO_ERROR_NO_CONNECTION,
+    /// A socket address could not be used.
+    NSTD_IO_ERROR_SOCKET_IN_USE,
+    /// An address could not be found.
+    NSTD_IO_ERROR_ADDRESS_NOT_FOUND,
+    /// The operation failed because a pipe was closed.
+    NSTD_IO_ERROR_BROKEN_PIPE,
+    /// An entity, such as a file, already exists.
+    NSTD_IO_ERROR_ALREADY_EXISTS,
+    /// The operation needs to block to complete.
+    NSTD_IO_ERROR_BLOCKING,
+    /// Some input parameter was incorrect.
+    NSTD_IO_ERROR_INVALID_INPUT,
+    /// Some input data was incorrect.
+    NSTD_IO_ERROR_INVALID_DATA,
+    /// The I/O operation's timeout expired, causing it to be canceled.
+    NSTD_IO_ERROR_TIMED_OUT,
+    /// Zero bytes were written to an output stream.
+    NSTD_IO_ERROR_WRITE_ZERO,
+    /// The operation was interrupted.
+    NSTD_IO_ERROR_INTERRUPTED,
+    /// The operation is unsupported on the current platform.
+    NSTD_IO_ERROR_UNSUPPORTED,
+    /// A reader unexpectedly reached the end of a file.
+    NSTD_IO_ERROR_UNEXPECTED_EOF,
+    /// An operation could not be completed, because it failed to allocate enough memory.
+    NSTD_IO_ERROR_OUT_OF_MEMORY,
+} NSTDIOError;
+
 /// Writes a C string slice to stdout.
 ///
 /// # Parameters:
--- src/io.rs
+++ src/io.rs
@@ -4,7 +4,84 @@ use crate::{
     core::{cstr::NSTDCStrConst, def::NSTDErrorCode},
     string::{nstd_string_new, nstd_string_pop, NSTDString},
 };
-use std::io::{prelude::*, BufReader};
+use std::io::{prelude::*, BufReader, ErrorKind};
+
+/// An error type for I/O operations.
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+#[allow(non_camel_case_types)]
+pub enum NSTDIOError {
+    /// No error ocurred.
+    NSTD_IO_ERROR_NONE,
+    /// An unknown/other error ocurred.
+    NSTD_IO_ERROR_UNKNOWN,
+    /// An entity, such as a file, was not found.
+    NSTD_IO_ERROR_NOT_FOUND,
+    /// Permission was denied.
+    NSTD_IO_ERROR_PERMISSION_DENIED,
+    /// The connection was refused by a remote server.
+    NSTD_IO_ERROR_CONNECTION_REFUSED,
+    /// The connection was reset by a remote server.
+    NSTD_IO_ERROR_CONNECTION_RESET,
+    /// The connection was terminated by a remote server.
+    NSTD_IO_ERROR_CONNECTION_TERMINATED,
+    /// There is no connection.
+    NSTD_IO_ERROR_NO_CONNECTION,
+    /// A socket address could not be used.
+    NSTD_IO_ERROR_SOCKET_IN_USE,
+    /// An address could not be found.
+    NSTD_IO_ERROR_ADDRESS_NOT_FOUND,
+    /// The operation failed because a pipe was closed.
+    NSTD_IO_ERROR_BROKEN_PIPE,
+    /// An entity, such as a file, already exists.
+    NSTD_IO_ERROR_ALREADY_EXISTS,
+    /// The operation needs to block to complete.
+    NSTD_IO_ERROR_BLOCKING,
+    /// Some input parameter was incorrect.
+    NSTD_IO_ERROR_INVALID_INPUT,
+    /// Some input data was incorrect.
+    NSTD_IO_ERROR_INVALID_DATA,
+    /// The I/O operation's timeout expired, causing it to be canceled.
+    NSTD_IO_ERROR_TIMED_OUT,
+    /// Zero bytes were written to an output stream.
+    NSTD_IO_ERROR_WRITE_ZERO,
+    /// The operation was interrupted.
+    NSTD_IO_ERROR_INTERRUPTED,
+    /// The operation is unsupported on the current platform.
+    NSTD_IO_ERROR_UNSUPPORTED,
+    /// A reader unexpectedly reached the end of a file.
+    NSTD_IO_ERROR_UNEXPECTED_EOF,
+    /// An operation could not be completed, because it failed to allocate enough memory.
+    NSTD_IO_ERROR_OUT_OF_MEMORY,
+}
+impl NSTDIOError {
+    /// Creates a new instance of [NSTDIOError] from a Rust [ErrorKind].
+    #[allow(dead_code)]
+    pub(crate) fn from_err(err: ErrorKind) -> Self {
+        match err {
+            ErrorKind::NotFound => NSTDIOError::NSTD_IO_ERROR_NOT_FOUND,
+            ErrorKind::PermissionDenied => NSTDIOError::NSTD_IO_ERROR_PERMISSION_DENIED,
+            ErrorKind::ConnectionRefused => NSTDIOError::NSTD_IO_ERROR_CONNECTION_REFUSED,
+            ErrorKind::ConnectionReset => NSTDIOError::NSTD_IO_ERROR_CONNECTION_RESET,
+            ErrorKind::ConnectionAborted => NSTDIOError::NSTD_IO_ERROR_CONNECTION_TERMINATED,
+            ErrorKind::NotConnected => NSTDIOError::NSTD_IO_ERROR_NO_CONNECTION,
+            ErrorKind::AddrInUse => NSTDIOError::NSTD_IO_ERROR_SOCKET_IN_USE,
+            ErrorKind::AddrNotAvailable => NSTDIOError::NSTD_IO_ERROR_ADDRESS_NOT_FOUND,
+            ErrorKind::BrokenPipe => NSTDIOError::NSTD_IO_ERROR_BROKEN_PIPE,
+            ErrorKind::AlreadyExists => NSTDIOError::NSTD_IO_ERROR_ALREADY_EXISTS,
+            ErrorKind::WouldBlock => NSTDIOError::NSTD_IO_ERROR_BLOCKING,
+            ErrorKind::InvalidInput => NSTDIOError::NSTD_IO_ERROR_INVALID_INPUT,
+            ErrorKind::InvalidData => NSTDIOError::NSTD_IO_ERROR_INVALID_DATA,
+            ErrorKind::TimedOut => NSTDIOError::NSTD_IO_ERROR_TIMED_OUT,
+            ErrorKind::WriteZero => NSTDIOError::NSTD_IO_ERROR_WRITE_ZERO,
+            ErrorKind::Interrupted => NSTDIOError::NSTD_IO_ERROR_INTERRUPTED,
+            ErrorKind::Unsupported => NSTDIOError::NSTD_IO_ERROR_UNSUPPORTED,
+            ErrorKind::UnexpectedEof => NSTDIOError::NSTD_IO_ERROR_UNEXPECTED_EOF,
+            ErrorKind::OutOfMemory => NSTDIOError::NSTD_IO_ERROR_OUT_OF_MEMORY,
+            _ => NSTDIOError::NSTD_IO_ERROR_UNKNOWN,
+        }
+    }
+}
 
 /// Writes a C string slice to stdout.
 ///
