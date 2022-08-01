0b5ab4882d2f337d58ae4ab8c4e4d971fd3bd71b
RustEnthusiast <RustEnthusiast@gmail.com>
2022-07-31 21:25:52 -0500

    Added `nstd_io_stdout[_free]`.

--- include/nstd/io/stdout.h
+++ include/nstd/io/stdout.h
@@ -1,8 +1,24 @@
 #ifndef NSTD_IO_STDOUT_H_INCLUDED
 #define NSTD_IO_STDOUT_H_INCLUDED
 #include "../nstd.h"
+NSTDCPPSTART
 
 /// A handle to the standard output stream.
 typedef NSTDAnyMut NSTDStdout;
 
+/// Constructs a new handle to the standard output stream.
+///
+/// # Returns
+///
+/// `NSTDStdout stdout` - A locked handle to the standard output stream.
+NSTDAPI NSTDStdout nstd_io_stdout();
+
+/// Frees and unlocks an instance of `NSTDStdout`.
+///
+/// # Parameters:
+///
+/// - `NSTDStdout stdout` - A handle to the standard output stream.
+NSTDAPI void nstd_io_stdout_free(NSTDStdout stdout);
+
+NSTDCPPEND
 #endif
--- src/io/stdout.rs
+++ src/io/stdout.rs
@@ -3,3 +3,24 @@ use std::io::Stdout;
 
 /// A handle to the standard output stream.
 pub type NSTDStdout = Box<Stdout>;
+
+/// Constructs a new handle to the standard output stream.
+///
+/// # Returns
+///
+/// `NSTDStdout stdout` - A locked handle to the standard output stream.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_io_stdout() -> NSTDStdout {
+    NSTDStdout::new(std::io::stdout())
+}
+
+/// Frees and unlocks an instance of `NSTDStdout`.
+///
+/// # Parameters:
+///
+/// - `NSTDStdout stdout` - A handle to the standard output stream.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_io_stdout_free(stdout: NSTDStdout) {}
