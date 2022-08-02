e90d0fa732939542aa5f4fab016323e56cd5faca
RustEnthusiast <RustEnthusiast@gmail.com>
2022-08-02 14:00:13 -0500

    Added `nstd_io_stdin[_free]`.

--- include/nstd/io/stdin.h
+++ include/nstd/io/stdin.h
@@ -1,8 +1,24 @@
 #ifndef NSTD_IO_STDIN_H_INCLUDED
 #define NSTD_IO_STDIN_H_INCLUDED
 #include "../nstd.h"
+NSTDCPPSTART
 
 /// A handle to the standard input stream.
 typedef NSTDAnyMut NSTDStdin;
 
+/// Constructs a new handle to the standard input stream.
+///
+/// # Returns
+///
+/// `NSTDStdin handle` - A handle to the standard input stream.
+NSTDAPI NSTDStdin nstd_io_stdin();
+
+/// Frees an instance of `NSTDStdin`.
+///
+/// # Parameters:
+///
+/// - `NSTDStdin handle` - A handle to the standard input stream.
+NSTDAPI void nstd_io_stdin_free(NSTDStdin handle);
+
+NSTDCPPEND
 #endif
--- src/io/stdin.rs
+++ src/io/stdin.rs
@@ -3,3 +3,24 @@ use std::io::Stdin;
 
 /// A handle to the standard input stream.
 pub type NSTDStdin = Box<Stdin>;
+
+/// Constructs a new handle to the standard input stream.
+///
+/// # Returns
+///
+/// `NSTDStdin handle` - A handle to the standard input stream.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_io_stdin() -> NSTDStdin {
+    NSTDStdin::new(std::io::stdin())
+}
+
+/// Frees an instance of `NSTDStdin`.
+///
+/// # Parameters:
+///
+/// - `NSTDStdin handle` - A handle to the standard input stream.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_io_stdin_free(handle: NSTDStdin) {}
