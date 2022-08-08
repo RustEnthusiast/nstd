c520fc012edfe4860725bc201e06a5615e5fa8df
RustEnthusiast <RustEnthusiast@gmail.com>
2022-08-08 14:03:56 -0500

    Added `NSTDWindow`.

--- include/nstd/window.h
+++ include/nstd/window.h
@@ -1,4 +1,33 @@
 #ifndef NSTD_WINDOW_H
 #define NSTD_WINDOW_H
+#include "app/handle.h"
 #include "nstd.h"
+NSTDCPPSTART
+
+/// An `nstd` application window.
+typedef NSTDAnyMut NSTDWindow;
+
+/// Creates a new window attached to `app`'s event loop.
+///
+/// # Parameters:
+///
+/// - `NSTDAppHandle app` - A handle to an `nstd` application.
+///
+/// # Returns
+///
+/// `NSTDWindow window` - A handle to the newly created window.
+///
+/// # Panics
+///
+/// This operation will panic if creating the new window fails.
+NSTDAPI NSTDWindow nstd_window_new(NSTDAppHandle app);
+
+/// Permanently closes & frees a window and it's data.
+///
+/// # Parameters:
+///
+/// - `NSTDWindow window` - The window to close.
+NSTDAPI void nstd_window_close(NSTDWindow window);
+
+NSTDCPPEND
 #endif
--- src/window.rs
+++ src/window.rs
@@ -1,1 +1,35 @@
 //! An `nstd` application window.
+use crate::app::handle::NSTDAppHandle;
+use winit::window::Window;
+
+/// An `nstd` application window.
+pub type NSTDWindow = Box<Window>;
+
+/// Creates a new window attached to `app`'s event loop.
+///
+/// # Parameters:
+///
+/// - `NSTDAppHandle app` - A handle to an `nstd` application.
+///
+/// # Returns
+///
+/// `NSTDWindow window` - A handle to the newly created window.
+///
+/// # Panics
+///
+/// This operation will panic if creating the new window fails.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_window_new(app: NSTDAppHandle) -> NSTDWindow {
+    Box::new(Window::new(app).expect("Failed to create an nstd application window."))
+}
+
+/// Permanently closes & frees a window and it's data.
+///
+/// # Parameters:
+///
+/// - `NSTDWindow window` - The window to close.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_window_close(window: NSTDWindow) {}
