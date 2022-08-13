476d4f13841c082fa53ce329fba8ae799849a48c
RustEnthusiast <RustEnthusiast@gmail.com>
2022-08-12 20:29:29 -0500

    Added `NSTDImage`.

--- include/nstd/image.h
+++ include/nstd/image.h
@@ -1,4 +1,62 @@
 #ifndef NSTD_IMAGE_H
 #define NSTD_IMAGE_H
+#include "core/slice.h"
+#include "io/io.h"
 #include "nstd.h"
+NSTDCPPSTART
+
+/// An image of any format.
+typedef NSTDAnyMut NSTDImage;
+
+/// An enumeration that describes each type of `nstd.image` error.
+typedef enum {
+    /// An unknown image operation error.
+    NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
+    /// An I/O operation failed.
+    NSTD_IMAGE_ERROR_TYPE_IO,
+} NSTDImageErrorType;
+
+/// A union of each possible error from `nstd.image`.
+typedef union {
+    /// I/O operation error data.
+    NSTDIOError io_data;
+} NSTDImageErrorData;
+
+/// An error returned from an `nstd.image` function.
+typedef struct {
+    /// Indicates the type of error that has occurred.
+    NSTDImageErrorType errc;
+    /// A union of each possible error.
+    NSTDImageErrorData err;
+} NSTDImageError;
+
+/// Loads an image from an in-memory buffer.
+///
+/// # Parameters:
+///
+/// - `const NSTDSliceConst *buffer` - The image buffer.
+///
+/// - `NSTDImageError *errc` - Returns as the image operation's error code.
+///
+/// # Returns
+///
+/// `NSTDImage img` - The new image, or null on error.
+///
+/// # Panics
+///
+/// This operation will panic if `buffer`'s stride is not 1.
+///
+/// # Safety
+///
+/// This operation can cause undefined behavior if `buffer`'s data is invalid.
+NSTDAPI NSTDImage nstd_image_load(const NSTDSliceConst *buffer, NSTDImageError *errc);
+
+/// Frees image data.
+///
+/// # Parameters:
+///
+/// - `NSTDImage img` - The image.
+NSTDAPI void nstd_image_free(NSTDImage img);
+
+NSTDCPPEND
 #endif
--- src/image.rs
+++ src/image.rs
@@ -1,1 +1,96 @@
 //! Multi-format image processing.
+use crate::{core::slice::NSTDSliceConst, io::NSTDIOError};
+use image::{error::ImageError, DynamicImage};
+
+/// An image of any format.
+pub type NSTDImage = Box<DynamicImage>;
+
+/// An enumeration that describes each type of `nstd.image` error.
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub enum NSTDImageErrorType {
+    /// An unknown image operation error.
+    NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
+    /// An I/O operation failed.
+    NSTD_IMAGE_ERROR_TYPE_IO,
+}
+
+/// A union of each possible error from `nstd.image`.
+#[repr(C)]
+pub union NSTDImageErrorData {
+    /// No data.
+    empty: (),
+    /// I/O operation error data.
+    pub io_data: NSTDIOError,
+}
+
+/// An error returned from an `nstd.image` function.
+#[repr(C)]
+pub struct NSTDImageError {
+    /// Indicates the type of error that has occurred.
+    pub errc: NSTDImageErrorType,
+    /// A union of each possible error.
+    pub err: NSTDImageErrorData,
+}
+impl NSTDImageError {
+    /// Creates an instance of [NSTDImageError] from an [ImageError].
+    fn from_err(err: ImageError) -> Self {
+        match err {
+            // An I/O operation failed.
+            ImageError::IoError(err) => Self {
+                errc: NSTDImageErrorType::NSTD_IMAGE_ERROR_TYPE_IO,
+                err: NSTDImageErrorData {
+                    io_data: NSTDIOError::from_err(err.kind()),
+                },
+            },
+            // Another error occurred.
+            _ => Self {
+                errc: NSTDImageErrorType::NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
+                err: NSTDImageErrorData { empty: () },
+            },
+        }
+    }
+}
+
+/// Loads an image from an in-memory buffer.
+///
+/// # Parameters:
+///
+/// - `const NSTDSliceConst *buffer` - The image buffer.
+///
+/// - `NSTDImageError *errc` - Returns as the image operation's error code.
+///
+/// # Returns
+///
+/// `NSTDImage img` - The new image, or null on error.
+///
+/// # Panics
+///
+/// This operation will panic if `buffer`'s stride is not 1.
+///
+/// # Safety
+///
+/// This operation can cause undefined behavior if `buffer`'s data is invalid.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_image_load(
+    buffer: &NSTDSliceConst,
+    errc: &mut NSTDImageError,
+) -> Option<NSTDImage> {
+    match image::load_from_memory(buffer.as_slice()) {
+        Ok(img) => Some(Box::new(img)),
+        Err(err) => {
+            *errc = NSTDImageError::from_err(err);
+            None
+        }
+    }
+}
+
+/// Frees image data.
+///
+/// # Parameters:
+///
+/// - `NSTDImage img` - The image.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_image_free(img: NSTDImage) {}
