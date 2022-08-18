1eefb4c7396dfdedf1d4928e4af68b642ad67d76
RustEnthusiast <RustEnthusiast@gmail.com>
2022-08-18 12:46:22 -0500

    Added `nstd_thread_spawn_with_desc`.

--- include/nstd/thread.h
+++ include/nstd/thread.h
@@ -1,11 +1,29 @@
 #ifndef NSTD_THREAD_H
 #define NSTD_THREAD_H
 #include "core/def.h"
+#include "core/str.h"
 #include "nstd.h"
 NSTDCPPSTART
 
 /// A handle to a running thread.
 typedef NSTDAnyMut NSTDThread;
+
+/// Describes the creation of a new thread.
+///
+/// This type is passed to the `nstd_thread_spawn_with_desc` function.
+typedef struct {
+    /// The name of the thread.
+    NSTDStrConst name;
+    /// A pointer to the data to be passed to the thread.
+    ///
+    /// # Note
+    ///
+    /// This will only be passed to the thread function on platforms that support atomic pointers,
+    /// on other platforms `NSTD_NULL` will be passed.
+    NSTDAnyMut data;
+    /// The number of bytes that the thread's stack should have.
+    NSTDUSize stack_size;
+} NSTDThreadDescriptor;
 
 /// Spawns a new thread and returns a handle to it.
 ///
@@ -19,7 +37,29 @@ typedef NSTDAnyMut NSTDThread;
 /// # Returns
 ///
 /// `NSTDThread thread` - A handle to the new thread.
+///
+/// # Safety
+///
+/// The caller of this function must guarantee that `thread_fn` is a valid function pointer.
 NSTDAPI NSTDThread nstd_thread_spawn(NSTDErrorCode (*thread_fn)(NSTDAnyMut), NSTDAnyMut data);
+
+/// Spawns a new thread configured with a descriptor.
+///
+/// # Parameters:
+///
+/// - `NSTDErrorCode (*thread_fn)(NSTDAnyMut)` - The thread function.
+///
+/// - `const NSTDThreadDescriptor *desc` - The thread descriptor.
+///
+/// # Returns
+///
+/// `NSTDThread thread` - A handle to the new thread.
+///
+/// # Safety
+///
+/// This operation can cause undefined behavior if `desc`'s data is invalid.
+NSTDAPI NSTDThread nstd_thread_spawn_with_desc(NSTDErrorCode (*thread_fn)(NSTDAnyMut),
+const NSTDThreadDescriptor *desc);
 
 /// Puts the current thread to sleep for a specified number of seconds.
 ///
--- src/thread.rs
+++ src/thread.rs
@@ -1,9 +1,34 @@
 //! Thread spawning, joining, and detaching.
-use crate::{core::def::NSTDErrorCode, NSTDAnyMut, NSTDFloat64};
-use std::{thread::JoinHandle, time::Duration};
+use crate::{
+    core::{def::NSTDErrorCode, str::NSTDStrConst},
+    NSTDAnyMut, NSTDFloat64, NSTDUSize,
+};
+use std::{
+    thread::{Builder, JoinHandle},
+    time::Duration,
+};
 
 /// A handle to a running thread.
 pub type NSTDThread = Box<JoinHandle<NSTDErrorCode>>;
+
+/// Describes the creation of a new thread.
+///
+/// This type is passed to the `nstd_thread_spawn_with_desc` function.
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct NSTDThreadDescriptor {
+    /// The name of the thread.
+    pub name: NSTDStrConst,
+    /// A pointer to the data to be passed to the thread.
+    ///
+    /// # Note
+    ///
+    /// This will only be passed to the thread function on platforms that support atomic pointers,
+    /// on other platforms `NSTD_NULL` will be passed.
+    pub data: NSTDAnyMut,
+    /// The number of bytes that the thread's stack should have.
+    pub stack_size: NSTDUSize,
+}
 
 /// Spawns a new thread and returns a handle to it.
 ///
@@ -17,9 +42,13 @@ pub type NSTDThread = Box<JoinHandle<NSTDErrorCode>>;
 /// # Returns
 ///
 /// `NSTDThread thread` - A handle to the new thread.
+///
+/// # Safety
+///
+/// The caller of this function must guarantee that `thread_fn` is a valid function pointer.
 #[cfg_attr(feature = "clib", no_mangle)]
 #[cfg_attr(not(target_has_atomic = "ptr"), allow(unused_variables))]
-pub extern "C" fn nstd_thread_spawn(
+pub unsafe extern "C" fn nstd_thread_spawn(
     thread_fn: Option<unsafe extern "C" fn(NSTDAnyMut) -> NSTDErrorCode>,
     data: NSTDAnyMut,
 ) -> Option<NSTDThread> {
@@ -28,16 +57,63 @@ pub extern "C" fn nstd_thread_spawn(
         {
             use std::sync::atomic::AtomicPtr;
             let data = AtomicPtr::new(data);
-            return Some(Box::new(std::thread::spawn(move || unsafe {
+            return Some(Box::new(std::thread::spawn(move || {
                 thread_fn(data.into_inner())
             })));
         }
         #[cfg(not(target_has_atomic = "ptr"))]
         {
             use crate::NSTD_NULL;
-            return Some(Box::new(std::thread::spawn(move || unsafe {
-                thread_fn(NSTD_NULL)
-            })));
+            return Some(Box::new(std::thread::spawn(move || thread_fn(NSTD_NULL))));
+        }
+    }
+    None
+}
+
+/// Spawns a new thread configured with a descriptor.
+///
+/// # Parameters:
+///
+/// - `NSTDErrorCode (*thread_fn)(NSTDAnyMut)` - The thread function.
+///
+/// - `const NSTDThreadDescriptor *desc` - The thread descriptor.
+///
+/// # Returns
+///
+/// `NSTDThread thread` - A handle to the new thread.
+///
+/// # Safety
+///
+/// This operation can cause undefined behavior if `desc`'s data is invalid.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_thread_spawn_with_desc(
+    thread_fn: Option<unsafe extern "C" fn(NSTDAnyMut) -> NSTDErrorCode>,
+    desc: &NSTDThreadDescriptor,
+) -> Option<NSTDThread> {
+    if let Some(thread_fn) = thread_fn {
+        // Create the thread builder.
+        let mut builder = Builder::new();
+        builder = builder.name(desc.name.as_str().to_string());
+        if desc.stack_size != 0 {
+            builder = builder.stack_size(desc.stack_size);
+        }
+        // Spawn the new thread.
+        #[cfg(target_has_atomic = "ptr")]
+        {
+            use std::sync::atomic::AtomicPtr;
+            let data = AtomicPtr::new(desc.data);
+            return match builder.spawn(move || thread_fn(data.into_inner())) {
+                Ok(thread) => Some(Box::new(thread)),
+                _ => None,
+            };
+        }
+        #[cfg(not(target_has_atomic = "ptr"))]
+        {
+            use crate::NSTD_NULL;
+            return match builder.spawn(move || thread_fn(NSTD_NULL)) {
+                Ok(thread) => return Some(Box::new(thread)),
+                _ => None,
+            };
         }
     }
     None
