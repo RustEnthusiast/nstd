88f0be3eae9e66f1f24b570b17ec685fedd01992
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-08-25 16:47:02 -0500

    Dispatch `start` on `StartCause::Init`.

--- src/app.rs
+++ src/app.rs
@@ -9,7 +9,7 @@ use self::{
 };
 use crate::NSTDAnyMut;
 use winit::{
-    event::{DeviceEvent, ElementState, Event, WindowEvent},
+    event::{DeviceEvent, ElementState, Event, StartCause, WindowEvent},
     event_loop::EventLoop,
 };
 
@@ -92,21 +92,22 @@ pub extern "C" fn nstd_app_events(app: &mut NSTDApp) -> &mut NSTDAppEvents {
 /// This function's caller must guarantee validity of the `app`'s event callbacks.
 #[cfg_attr(feature = "clib", no_mangle)]
 pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
-    // Dispatch the `start` event.
-    if let Some(start) = app.events.start {
-        let start_app_data = NSTDAppData::new(&app.event_loop, data);
-        start(&start_app_data);
-    }
     // Run the winit event loop.
     app.event_loop.run(move |event, handle, control_flow| {
         // Instantiate a new instance of `NSTDAppData`.
-        let app_data = NSTDAppData::new(handle, data);
+        let ref app_data = NSTDAppData::new(handle, data);
         // Dispatch events.
         match event {
+            // The event loop was just started.
+            Event::NewEvents(StartCause::Init) => {
+                if let Some(start) = app.events.start {
+                    start(app_data);
+                }
+            }
             // All other events have been processed.
             Event::MainEventsCleared => {
                 if let Some(update) = app.events.update {
-                    update(&app_data);
+                    update(app_data);
                 }
             }
             // A device event was received.
@@ -114,39 +115,39 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                 // A device was connected to the system.
                 DeviceEvent::Added => {
                     if let Some(device_added) = app.events.device_added {
-                        device_added(&app_data, &device_id);
+                        device_added(app_data, &device_id);
                     }
                 }
                 // A device was disconnected from the system.
                 DeviceEvent::Removed => {
                     if let Some(device_removed) = app.events.device_removed {
-                        device_removed(&app_data, &device_id);
+                        device_removed(app_data, &device_id);
                     }
                 }
                 // A mouse device was moved.
                 DeviceEvent::MouseMotion { delta } => {
                     if let Some(mouse_moved) = app.events.mouse_moved {
-                        mouse_moved(&app_data, &device_id, delta.0, -delta.1);
+                        mouse_moved(app_data, &device_id, delta.0, -delta.1);
                     }
                 }
                 // A scroll wheel was scrolled.
                 DeviceEvent::MouseWheel { delta } => {
                     if let Some(mouse_scrolled) = app.events.mouse_scrolled {
                         let (x, y, delta_t) = NSTDScrollDelta::from_winit(delta);
-                        mouse_scrolled(&app_data, &device_id, x, y, delta_t);
+                        mouse_scrolled(app_data, &device_id, x, y, delta_t);
                     }
                 }
                 // There was motion on some analog axis.
                 DeviceEvent::Motion { axis, value } => {
                     if let Some(axis_motion) = app.events.axis_motion {
-                        axis_motion(&app_data, &device_id, &axis, value);
+                        axis_motion(app_data, &device_id, &axis, value);
                     }
                 }
                 // A button's state was changed.
                 DeviceEvent::Button { button, state } => {
                     if let Some(button_input) = app.events.button_input {
                         let is_down = state == ElementState::Pressed;
-                        button_input(&app_data, &device_id, &button, is_down.into());
+                        button_input(app_data, &device_id, &button, is_down.into());
                     }
                 }
                 // There was some keyboard input.
@@ -154,7 +155,7 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                     if let Some(key_input) = app.events.key_input {
                         let key = NSTDKey::from_winit(input.virtual_keycode);
                         let is_down = input.state == ElementState::Pressed;
-                        key_input(&app_data, &device_id, key, input.scancode, is_down.into());
+                        key_input(app_data, &device_id, key, input.scancode, is_down.into());
                     }
                 }
                 _ => (),
@@ -168,7 +169,7 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                 } => {
                     if let Some(window_dpi_changed) = app.events.window_dpi_changed {
                         window_dpi_changed(
-                            &app_data,
+                            app_data,
                             &window_id,
                             scale_factor,
                             &mut new_inner_size.width,
@@ -179,19 +180,19 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                 // A window was resized.
                 WindowEvent::Resized(size) => {
                     if let Some(window_resized) = app.events.window_resized {
-                        window_resized(&app_data, &window_id, size.width, size.height);
+                        window_resized(app_data, &window_id, size.width, size.height);
                     }
                 }
                 // A window was moved.
                 WindowEvent::Moved(pos) => {
                     if let Some(window_moved) = app.events.window_moved {
-                        window_moved(&app_data, &window_id, pos.x, pos.y);
+                        window_moved(app_data, &window_id, pos.x, pos.y);
                     }
                 }
                 // A window's focus has changed.
                 WindowEvent::Focused(is_focused) => {
                     if let Some(window_focus_changed) = app.events.window_focus_changed {
-                        window_focus_changed(&app_data, &window_id, is_focused.into());
+                        window_focus_changed(app_data, &window_id, is_focused.into());
                     }
                 }
                 // A window received mouse button input.
@@ -204,7 +205,7 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                     if let Some(window_mouse_input) = app.events.window_mouse_input {
                         let input = NSTDMouseInput::from_winit(button);
                         let is_down = (state == ElementState::Pressed).into();
-                        window_mouse_input(&app_data, &window_id, &device_id, &input, is_down);
+                        window_mouse_input(app_data, &window_id, &device_id, &input, is_down);
                     }
                 }
                 // A window received key input.
@@ -215,13 +216,13 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                         let key = NSTDKey::from_winit(input.virtual_keycode);
                         let is_down = (input.state == ElementState::Pressed).into();
                         let scancode = input.scancode;
-                        window_key_input(&app_data, &window_id, &device_id, key, scancode, is_down);
+                        window_key_input(app_data, &window_id, &device_id, key, scancode, is_down);
                     }
                 }
                 // A window received character input.
                 WindowEvent::ReceivedCharacter(chr) => {
                     if let Some(window_received_char) = app.events.window_received_char {
-                        window_received_char(&app_data, &window_id, chr.into());
+                        window_received_char(app_data, &window_id, chr.into());
                     }
                 }
                 // A window was scrolled.
@@ -234,7 +235,7 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                     if let Some(window_scrolled) = app.events.window_scrolled {
                         let (x, y, delta_t) = NSTDScrollDelta::from_winit(delta);
                         let touch = NSTDTouchState::from_winit(phase);
-                        window_scrolled(&app_data, &window_id, &device_id, x, y, delta_t, touch);
+                        window_scrolled(app_data, &window_id, &device_id, x, y, delta_t, touch);
                     }
                 }
                 // The cursor was moved over a window.
@@ -244,31 +245,31 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
                     ..
                 } => {
                     if let Some(window_cursor_moved) = app.events.window_cursor_moved {
-                        window_cursor_moved(&app_data, &window_id, &device_id, pos.x, pos.y);
+                        window_cursor_moved(app_data, &window_id, &device_id, pos.x, pos.y);
                     }
                 }
                 // The cursor entered a window.
                 WindowEvent::CursorEntered { device_id } => {
                     if let Some(window_cursor_entered) = app.events.window_cursor_entered {
-                        window_cursor_entered(&app_data, &window_id, &device_id);
+                        window_cursor_entered(app_data, &window_id, &device_id);
                     }
                 }
                 // The cursor left a window.
                 WindowEvent::CursorLeft { device_id } => {
                     if let Some(window_cursor_left) = app.events.window_cursor_left {
-                        window_cursor_left(&app_data, &window_id, &device_id);
+                        window_cursor_left(app_data, &window_id, &device_id);
                     }
                 }
                 // A window requests closing.
                 WindowEvent::CloseRequested => {
                     if let Some(window_close_requested) = app.events.window_close_requested {
-                        window_close_requested(&app_data, &window_id);
+                        window_close_requested(app_data, &window_id);
                     }
                 }
                 // A window was permanently closed.
                 WindowEvent::Destroyed => {
                     if let Some(window_closed) = app.events.window_closed {
-                        window_closed(&app_data, &window_id);
+                        window_closed(app_data, &window_id);
                     }
                 }
                 _ => (),
@@ -276,13 +277,13 @@ pub unsafe extern "C" fn nstd_app_run(app: NSTDApp, data: NSTDAnyMut) -> ! {
             // The event loop is being exited.
             Event::LoopDestroyed => {
                 if let Some(exit) = app.events.exit {
-                    exit(&app_data);
+                    exit(app_data);
                 }
             }
             _ => (),
         }
         // Set the `winit` event loop's control flow.
-        *control_flow = app_data.control_flow.into_inner().into();
+        *control_flow = app_data.control_flow.get().into();
     })
 }
 
