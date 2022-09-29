8b3031e9f9bba4945a35ac9132511632da0b0916
RustEnthusiast <RustEnthusiast@gmail.com>
2022-09-29 09:33:54 -0500

    Added examples to `nstd.os.windows.alloc`.

--- include/nstd/os/windows/alloc/alloc.h
+++ include/nstd/os/windows/alloc/alloc.h
@@ -16,6 +16,30 @@
 /// # Safety
 ///
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapalloc>.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     core::mem::nstd_core_mem_zero,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[u32; 5]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate(SIZE);
+///     assert!(!buf.is_null());
+///
+///     nstd_core_mem_zero(buf.cast(), SIZE);
+///     assert!(*buf.cast::<[u32; 5]>() == [0u32; 5]);
+///
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 NSTDAPI NSTDAnyMut nstd_os_windows_alloc_allocate(NSTDUInt size);
 
 /// Allocates a new block of zero-initialized memory on the current process' heap.
@@ -31,6 +55,26 @@ NSTDAPI NSTDAnyMut nstd_os_windows_alloc_allocate(NSTDUInt size);
 /// # Safety
 ///
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapalloc>.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate_zeroed, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[i64; 5]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate_zeroed(SIZE);
+///     assert!(!buf.is_null());
+///     assert!(*buf.cast::<[i64; 5]>() == [0i64; 5]);
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 NSTDAPI NSTDAnyMut nstd_os_windows_alloc_allocate_zeroed(NSTDUInt size);
 
 /// Reallocates a block of memory previously allocated by
@@ -53,6 +97,31 @@ NSTDAPI NSTDAnyMut nstd_os_windows_alloc_allocate_zeroed(NSTDUInt size);
 /// # Safety
 ///
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heaprealloc>.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate_zeroed, nstd_os_windows_alloc_deallocate,
+///         nstd_os_windows_alloc_reallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[i16; 10]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate_zeroed(SIZE);
+///     assert!(!buf.is_null());
+///     assert!(*buf.cast::<[i16; 10]>() == [0i16; 10]);
+///
+///     assert!(nstd_os_windows_alloc_reallocate(&mut buf, SIZE / 2) == NSTD_ALLOC_ERROR_NONE);
+///     assert!(*buf.cast::<[i16; 5]>() == [0i16; 5]);
+///
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 NSTDAPI NSTDAllocError nstd_os_windows_alloc_reallocate(NSTDAnyMut *ptr, NSTDUInt new_size);
 
 /// Deallocates a block of memory previously allocated by
@@ -69,6 +138,23 @@ NSTDAPI NSTDAllocError nstd_os_windows_alloc_reallocate(NSTDAnyMut *ptr, NSTDUIn
 /// # Safety
 ///
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapfree>.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate(128);
+///     assert!(!buf.is_null());
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 NSTDAPI NSTDAllocError nstd_os_windows_alloc_deallocate(NSTDAnyMut *ptr);
 
 #endif
--- src/os/windows/alloc.rs
+++ src/os/windows/alloc.rs
@@ -26,6 +26,30 @@ use crate::{alloc::NSTDAllocError, NSTDAnyMut, NSTDUInt};
 /// - See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapalloc>.
 ///
 /// - This operation can cause undefined behavior if it panics into non-Rust code.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     core::mem::nstd_core_mem_zero,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[u32; 5]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate(SIZE);
+///     assert!(!buf.is_null());
+///
+///     nstd_core_mem_zero(buf.cast(), SIZE);
+///     assert!(*buf.cast::<[u32; 5]>() == [0u32; 5]);
+///
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 #[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
 pub unsafe extern "C" fn nstd_os_windows_alloc_allocate(size: NSTDUInt) -> NSTDAnyMut {
@@ -52,6 +76,26 @@ pub unsafe extern "C" fn nstd_os_windows_alloc_allocate(size: NSTDUInt) -> NSTDA
 /// - See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapalloc>.
 ///
 /// - This operation can cause undefined behavior if it panics into non-Rust code.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate_zeroed, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[i64; 5]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate_zeroed(SIZE);
+///     assert!(!buf.is_null());
+///     assert!(*buf.cast::<[i64; 5]>() == [0i64; 5]);
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 #[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
 pub unsafe extern "C" fn nstd_os_windows_alloc_allocate_zeroed(size: NSTDUInt) -> NSTDAnyMut {
@@ -85,6 +129,31 @@ pub unsafe extern "C" fn nstd_os_windows_alloc_allocate_zeroed(size: NSTDUInt) -
 /// - See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heaprealloc>.
 ///
 /// - This operation can cause undefined behavior if it panics into non-Rust code.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate_zeroed, nstd_os_windows_alloc_deallocate,
+///         nstd_os_windows_alloc_reallocate,
+///     },
+/// };
+///
+/// const SIZE: usize = core::mem::size_of::<[i16; 10]>();
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate_zeroed(SIZE);
+///     assert!(!buf.is_null());
+///     assert!(*buf.cast::<[i16; 10]>() == [0i16; 10]);
+///
+///     assert!(nstd_os_windows_alloc_reallocate(&mut buf, SIZE / 2) == NSTD_ALLOC_ERROR_NONE);
+///     assert!(*buf.cast::<[i16; 5]>() == [0i16; 5]);
+///
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 #[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
 pub unsafe extern "C" fn nstd_os_windows_alloc_reallocate(
@@ -115,6 +184,23 @@ pub unsafe extern "C" fn nstd_os_windows_alloc_reallocate(
 /// - See <https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapfree>.
 ///
 /// - This operation can cause undefined behavior if it panics into non-Rust code.
+///
+/// # Example
+///
+/// ```
+/// use nstd_sys::{
+///     alloc::NSTDAllocError::NSTD_ALLOC_ERROR_NONE,
+///     os::windows::alloc::{
+///         nstd_os_windows_alloc_allocate, nstd_os_windows_alloc_deallocate,
+///     },
+/// };
+///
+/// unsafe {
+///     let mut buf = nstd_os_windows_alloc_allocate(128);
+///     assert!(!buf.is_null());
+///     assert!(nstd_os_windows_alloc_deallocate(&mut buf) == NSTD_ALLOC_ERROR_NONE);
+/// }
+/// ```
 #[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
 pub unsafe extern "C" fn nstd_os_windows_alloc_deallocate(ptr: &mut NSTDAnyMut) -> NSTDAllocError {
