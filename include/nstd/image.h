b24b7279b1a59854d6ce34d80686e89f2a87769e
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-10-13 09:46:46 -0500

    Updated `nstd_image_load`.

--- include/nstd/image.h
+++ include/nstd/image.h
@@ -1,33 +1,10 @@
 #ifndef NSTD_IMAGE_H
 #define NSTD_IMAGE_H
 #include "core/slice.h"
-#include "io/io.h"
 #include "nstd.h"
 
 /// An image of any format.
 typedef NSTDAnyMut NSTDImage;
-
-/// An enumeration that describes each type of `nstd.image` error.
-typedef enum {
-    /// An unknown image operation error.
-    NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
-    /// An I/O operation failed.
-    NSTD_IMAGE_ERROR_TYPE_IO,
-} NSTDImageErrorType;
-
-/// A union of each possible error from `nstd.image`.
-typedef union {
-    /// I/O operation error data.
-    NSTDIOError io_data;
-} NSTDImageErrorData;
-
-/// An error returned from an `nstd.image` function.
-typedef struct {
-    /// Indicates the type of error that has occurred.
-    NSTDImageErrorType errc;
-    /// A union of each possible error.
-    NSTDImageErrorData err;
-} NSTDImageError;
 
 /// Loads an image from an in-memory buffer.
 ///
@@ -35,20 +12,22 @@ typedef struct {
 ///
 /// - `const NSTDSlice *buffer` - The image buffer.
 ///
-/// - `NSTDImageError *errc` - Returns as the image operation's error code.
-///
 /// # Returns
 ///
 /// `NSTDImage img` - The new image, or null on error.
 ///
 /// # Panics
 ///
-/// This operation will panic if `buffer`'s stride is not 1.
+/// This operation will panic in the following events:
+///
+/// - `buffer`'s stride is not 1.
+///
+/// - `buffer`'s length is greater than `NSTDInt`'s max value.
 ///
 /// # Safety
 ///
 /// This operation can cause undefined behavior if `buffer`'s data is invalid.
-NSTDAPI NSTDImage nstd_image_load(const NSTDSlice *buffer, NSTDImageError *errc);
+NSTDAPI NSTDImage nstd_image_load(const NSTDSlice *buffer);
 
 /// Returns an image's raw pixel data as a byte slice.
 ///
--- src/image.rs
+++ src/image.rs
@@ -1,60 +1,12 @@
 //! Multi-format image processing.
 use crate::{
     core::slice::{nstd_core_slice_new, NSTDSlice},
-    io::NSTDIOError,
     NSTDUInt32,
 };
-use image::{error::ImageError, DynamicImage};
+use image::DynamicImage;
 
 /// An image of any format.
 pub type NSTDImage = Box<DynamicImage>;
-
-/// An enumeration that describes each type of `nstd.image` error.
-#[repr(C)]
-#[allow(non_camel_case_types)]
-pub enum NSTDImageErrorType {
-    /// An unknown image operation error.
-    NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
-    /// An I/O operation failed.
-    NSTD_IMAGE_ERROR_TYPE_IO,
-}
-
-/// A union of each possible error from `nstd.image`.
-#[repr(C)]
-pub union NSTDImageErrorData {
-    /// No data.
-    empty: (),
-    /// I/O operation error data.
-    pub io_data: NSTDIOError,
-}
-
-/// An error returned from an `nstd.image` function.
-#[repr(C)]
-pub struct NSTDImageError {
-    /// Indicates the type of error that has occurred.
-    pub errc: NSTDImageErrorType,
-    /// A union of each possible error.
-    pub err: NSTDImageErrorData,
-}
-impl NSTDImageError {
-    /// Creates an instance of [NSTDImageError] from an [ImageError].
-    fn from_err(err: ImageError) -> Self {
-        match err {
-            // An I/O operation failed.
-            ImageError::IoError(err) => Self {
-                errc: NSTDImageErrorType::NSTD_IMAGE_ERROR_TYPE_IO,
-                err: NSTDImageErrorData {
-                    io_data: NSTDIOError::from_err(err.kind()),
-                },
-            },
-            // Another error occurred.
-            _ => Self {
-                errc: NSTDImageErrorType::NSTD_IMAGE_ERROR_TYPE_UNKNOWN,
-                err: NSTDImageErrorData { empty: () },
-            },
-        }
-    }
-}
 
 /// Loads an image from an in-memory buffer.
 ///
@@ -62,30 +14,27 @@ impl NSTDImageError {
 ///
 /// - `const NSTDSlice *buffer` - The image buffer.
 ///
-/// - `NSTDImageError *errc` - Returns as the image operation's error code.
-///
 /// # Returns
 ///
 /// `NSTDImage img` - The new image, or null on error.
 ///
 /// # Panics
 ///
-/// This operation will panic if `buffer`'s stride is not 1.
+/// This operation will panic in the following events:
+///
+/// - `buffer`'s stride is not 1.
+///
+/// - `buffer`'s length is greater than `NSTDInt`'s max value.
 ///
 /// # Safety
 ///
 /// This operation can cause undefined behavior if `buffer`'s data is invalid.
+#[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
-pub unsafe extern "C" fn nstd_image_load(
-    buffer: &NSTDSlice,
-    errc: &mut NSTDImageError,
-) -> Option<NSTDImage> {
+pub unsafe extern "C" fn nstd_image_load(buffer: &NSTDSlice) -> Option<NSTDImage> {
     match image::load_from_memory(buffer.as_slice()) {
         Ok(img) => Some(Box::new(img)),
-        Err(err) => {
-            *errc = NSTDImageError::from_err(err);
-            None
-        }
+        _ => None,
     }
 }
 
