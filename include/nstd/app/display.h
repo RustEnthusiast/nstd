71f888e9cfbf0e6cf7b38e144b01fccf65266cdd
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-10-15 08:07:00 -0500

    Added `NSTDDisplayMode`.

--- include/nstd/app/display.h
+++ include/nstd/app/display.h
@@ -5,6 +5,9 @@
 /// A handle to a display.
 typedef NSTDAny NSTDDisplayHandle;
 
+/// Represents a display's video mode.
+typedef NSTDAny NSTDDisplayMode;
+
 /// Represents the size of a display.
 typedef struct {
     /// The width of the display.
@@ -64,5 +67,51 @@ NSTDAPI NSTDUInt32 nstd_app_display_refresh_rate(NSTDDisplayHandle display);
 ///
 /// `NSTDFloat64 scale_factor` - The display's scale factor.
 NSTDAPI NSTDFloat64 nstd_app_display_scale_factor(NSTDDisplayHandle display);
+
+/// Invokes a callback function for each display mode detected for a display.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayHandle display` - A handle to the display.
+///
+/// - `void (*callback)(NSTDDisplayMode)` - The callback function.
+///
+/// # Safety
+///
+/// The user of this function must guarantee that `callback` is a valid C function pointer.
+NSTDAPI void nstd_app_display_modes(NSTDDisplayHandle display, void (*callback)(NSTDDisplayMode));
+
+/// Returns the size of a display mode.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDDisplaySize size` - The display mode's size.
+NSTDAPI NSTDDisplaySize nstd_app_display_mode_size(NSTDDisplayMode mode);
+
+/// Returns the bit depth of a display mode.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDUInt16 bit_depth` - The display mode's bit depth.
+NSTDAPI NSTDUInt16 nstd_app_display_mode_bit_depth(NSTDDisplayMode mode);
+
+/// Returns the refresh rate of a display mode in millihertz.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDUInt32 refresh_rate` - The display's refresh rate.
+NSTDAPI NSTDUInt32 nstd_app_display_mode_refresh_rate(NSTDDisplayMode mode);
 
 #endif
--- src/app/display.rs
+++ src/app/display.rs
@@ -1,9 +1,12 @@
 //! Provides access to physical displays.
-use crate::{NSTDFloat64, NSTDInt32, NSTDUInt32};
-use winit::monitor::MonitorHandle;
+use crate::{NSTDFloat64, NSTDInt32, NSTDUInt16, NSTDUInt32};
+use winit::monitor::{MonitorHandle, VideoMode};
 
 /// A handle to a display.
 pub type NSTDDisplayHandle<'a> = &'a MonitorHandle;
+
+/// Represents a display's video mode.
+pub type NSTDDisplayMode<'a> = &'a VideoMode;
 
 /// Represents the size of a display.
 #[repr(C)]
@@ -91,4 +94,77 @@ pub extern "C" fn nstd_app_display_refresh_rate(display: NSTDDisplayHandle) -> N
 #[cfg_attr(feature = "clib", no_mangle)]
 pub extern "C" fn nstd_app_display_scale_factor(display: NSTDDisplayHandle) -> NSTDFloat64 {
     display.scale_factor()
+}
+
+/// Invokes a callback function for each display mode detected for a display.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayHandle display` - A handle to the display.
+///
+/// - `void (*callback)(NSTDDisplayMode)` - The callback function.
+///
+/// # Safety
+///
+/// The user of this function must guarantee that `callback` is a valid C function pointer.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_app_display_modes(
+    display: NSTDDisplayHandle,
+    callback: Option<unsafe extern "C" fn(NSTDDisplayMode)>,
+) {
+    if let Some(callback) = callback {
+        for mode in display.video_modes() {
+            callback(&mode);
+        }
+    }
+}
+
+/// Returns the size of a display mode.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDDisplaySize size` - The display mode's size.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_app_display_mode_size(mode: NSTDDisplayMode) -> NSTDDisplaySize {
+    let size = mode.size();
+    NSTDDisplaySize {
+        width: size.width,
+        height: size.height,
+    }
+}
+
+/// Returns the bit depth of a display mode.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDUInt16 bit_depth` - The display mode's bit depth.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_app_display_mode_bit_depth(mode: NSTDDisplayMode) -> NSTDUInt16 {
+    mode.bit_depth()
+}
+
+/// Returns the refresh rate of a display mode in millihertz.
+///
+/// # Parameters:
+///
+/// - `NSTDDisplayMode mode` - The display mode.
+///
+/// # Returns
+///
+/// `NSTDUInt32 refresh_rate` - The display's refresh rate.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_app_display_mode_refresh_rate(mode: NSTDDisplayMode) -> NSTDUInt32 {
+    mode.refresh_rate_millihertz()
 }
