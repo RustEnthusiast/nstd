fbd6ef2e37ac401bafe606a13209cfe05cd2cfcd
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-10-20 18:26:28 -0500

    Improved `Result` and `Optional`.

--- include/nstd/core/optional.h
+++ include/nstd/core/optional.h
@@ -1,10 +1,18 @@
 #ifndef NSTD_CORE_OPTIONAL_H
 #define NSTD_CORE_OPTIONAL_H
 
+/// Describes an `NSTDOptional` variant.
+typedef enum {
+    /// "Some" initialized value.
+    NSTD_OPTIONAL_STATUS_SOME,
+    /// No value.
+    NSTD_OPTIONAL_STATUS_NONE
+} NSTDOptionalStatus;
+
 /// Represents an optional (possibly uninitialized) value.
 #define NSTDOptional(T) typedef struct {\
-    enum { NSTD_OPTIONAL_SOME, NSTD_OPTIONAL_NONE } opt;\
-    union { T some; } value;\
+    NSTDOptionalStatus status;\
+    union { T some; };\
 }
 
 #endif
--- include/nstd/core/result.h
+++ include/nstd/core/result.h
@@ -1,10 +1,18 @@
 #ifndef NSTD_CORE_RESULT_H
 #define NSTD_CORE_RESULT_H
 
+/// Describes an `NSTDResult` variant.
+typedef enum {
+    /// A successful variant.
+    NSTD_RESULT_STATUS_OK,
+    /// An error variant.
+    NSTD_RESULT_STATUS_ERR
+} NSTDResultStatus;
+
 /// Defines a "result" type with success and error variants.
 #define NSTDResult(T, E) typedef struct {\
-    enum { NSTD_RESULT_OK, NSTD_RESULT_ERR } errc;\
-    union { T ok; E err; } value;\
+    NSTDResultStatus status;\
+    union { T ok; E err; };\
 }
 
 #endif
