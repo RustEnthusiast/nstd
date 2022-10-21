2188e4baa874271a5455b16716a1afa7e760b8cf
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-10-21 12:35:34 -0500

    [`nstd.os.windows.shared_lib`] Renamed `add_dir` to `set_dir`.

--- include/nstd/os/windows/shared_lib.h
+++ include/nstd/os/windows/shared_lib.h
@@ -13,12 +13,12 @@ typedef struct {
 /// An optional (possibly null) shared Windows library handle.
 NSTDOptional(NSTDWindowsSharedLib) NSTDWindowsOptionalSharedLib;
 
-/// Adds a directory to the system's search path used to load shared libraries.
+/// Sets a directory to the system's search path used to load shared libraries.
 ///
 /// # Parameters:
 ///
 /// - `const NSTDChar *path` - A path to a directory to search when looking for DLLs. Pass null to
-/// restore the default search order.
+/// restore the default search path.
 ///
 /// # Returns
 ///
@@ -27,7 +27,7 @@ NSTDOptional(NSTDWindowsSharedLib) NSTDWindowsOptionalSharedLib;
 /// # Safety
 ///
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-setdlldirectorya>.
-NSTDAPI NSTDErrorCode nstd_os_windows_shared_lib_add_dir(const NSTDChar *path);
+NSTDAPI NSTDErrorCode nstd_os_windows_shared_lib_set_dir(const NSTDChar *path);
 
 /// Loads a shared library/module by name.
 ///
--- src/os/windows/shared_lib.rs
+++ src/os/windows/shared_lib.rs
@@ -25,12 +25,12 @@ impl Drop for NSTDWindowsSharedLib {
 /// An optional (possibly null) shared Windows library handle.
 pub type NSTDWindowsOptionalSharedLib = NSTDOptional<NSTDWindowsSharedLib>;
 
-/// Adds a directory to the system's search path used to load shared libraries.
+/// Sets a directory to the system's search path used to load shared libraries.
 ///
 /// # Parameters:
 ///
 /// - `const NSTDChar *path` - A path to a directory to search when looking for DLLs. Pass null to
-/// restore the default search order.
+/// restore the default search path.
 ///
 /// # Returns
 ///
@@ -41,7 +41,7 @@ pub type NSTDWindowsOptionalSharedLib = NSTDOptional<NSTDWindowsSharedLib>;
 /// See <https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-setdlldirectorya>.
 #[inline]
 #[cfg_attr(feature = "clib", no_mangle)]
-pub unsafe extern "C" fn nstd_os_windows_shared_lib_add_dir(
+pub unsafe extern "C" fn nstd_os_windows_shared_lib_set_dir(
     path: *const NSTDChar,
 ) -> NSTDErrorCode {
     (SetDllDirectoryA(path.cast()) == 0).into()
