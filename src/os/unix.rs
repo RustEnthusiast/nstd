202f01af39043658f69f3c5fa000c635949d439b
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-10-21 20:15:00 -0500

    Added `NSTDUnixSharedLib`.

---
+++ src/os/unix.rs
@@ -0,0 +1,4 @@
+//! Low level Unix like operating system support.
+#[cfg(feature = "nstd_os_unix_shared_lib")]
+#[cfg_attr(doc_cfg, doc(cfg(feature = "nstd_os_unix_shared_lib")))]
+pub mod shared_lib;
