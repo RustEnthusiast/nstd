19841ffe7283515f939b3f42e8fec412441a613b
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-11-09 11:51:10 -0600

    Updated `nstd.os.unix.alloc` for macOS.

--- src/os/unix/macos/alloc.asm
+++ src/os/unix/macos/alloc.asm
@@ -6,7 +6,7 @@
 .globl _nstd_os_unix_alloc_allocate
 _nstd_os_unix_alloc_allocate:
     sub rsp, 8
-    call malloc
+    call _malloc
     add rsp, 8
     ret
 
@@ -14,7 +14,7 @@ _nstd_os_unix_alloc_allocate:
 _nstd_os_unix_alloc_allocate_zeroed:
     sub rsp, 8
     mov rsi, 1
-    call calloc
+    call _calloc
     add rsp, 8
     ret
 
@@ -23,7 +23,7 @@ _nstd_os_unix_alloc_reallocate:
     push rbx
     mov rbx, rdi
     mov rdi, [rdi]
-    call realloc
+    call _realloc
     test rax, rax
     jz 1f
     mov [rbx], rax
@@ -39,7 +39,7 @@ _nstd_os_unix_alloc_reallocate:
 _nstd_os_unix_alloc_deallocate:
     push rdi
     mov rdi, [rdi]
-    call free
+    call _free
     pop rdi
     mov qword ptr [rdi], 0
     ret
