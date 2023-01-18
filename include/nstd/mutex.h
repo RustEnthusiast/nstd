b09c8786136fbc9007ee25991e8ab7598cbcceb5
RustEnthusiast <RustEnthusiast@gmail.com>
2023-01-18 00:19:13 -0600

    Implemented `NSTDMutex`.

--- include/nstd/mutex.h
+++ include/nstd/mutex.h
@@ -1,3 +1,101 @@
 #ifndef NSTD_MUTEX_H
 #define NSTD_MUTEX_H
+#include "core/optional.h"
+#include "core/result.h"
+#include "heap_ptr.h"
+#include "nstd.h"
+
+/// A mutual exclusion primitive useful for protecting shared data.
+typedef NSTDAnyMut NSTDMutex;
+
+/// A guard providing access to a mutex's protected data.
+typedef NSTDAnyMut NSTDMutexGuard;
+
+/// A lock result returned from `nstd_mutex_lock` containing the mutex guard whether or not the
+/// data is poisoned.
+NSTDResult(NSTDMutexGuard, NSTDMutexGuard) NSTDMutexLockResult;
+
+/// An optional value of type `NSTDMutexLockResult`.
+///
+/// This type is returned from `nstd_mutex_try_lock` where the uninitialized variant means that the
+/// function would block.
+NSTDOptional(NSTDMutexLockResult) NSTDOptionalMutexLockResult;
+
+/// Creates a new mutual exclusion primitive.
+///
+/// # Parameters:
+///
+/// - `NSTDHeapPtr data` - The data to protect.
+///
+/// # Returns
+///
+/// `NSTDMutex mutex` - The new mutex protecting `data`.
+NSTDAPI NSTDMutex nstd_mutex_new(NSTDHeapPtr data);
+
+/// Waits for a mutex lock to become acquired, returning a guard wrapping the protected data.
+///
+/// Attempting to call this function on a thread that already owns the lock will either result in a
+/// panic or a deadlock.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutex *mutex` - The mutex to lock.
+///
+/// # Returns
+///
+/// `NSTDMutexLockResult guard` - A handle to the mutex's protected data.
+///
+/// # Panics
+///
+/// This operation may panic if the lock is already held by the current thread.
+NSTDAPI NSTDMutexLockResult nstd_mutex_lock(const NSTDMutex *mutex);
+
+/// Returns a pointer to a mutex's raw data.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutexGuard *guard` - A handle to the mutex's protected data.
+///
+/// # Returns
+///
+/// `NSTDAny data` - A pointer to the mutex's data.
+NSTDAPI NSTDAny nstd_mutex_get(const NSTDMutexGuard *guard);
+
+/// Returns a mutable pointer to a mutex's raw data.
+///
+/// # Parameters:
+///
+/// - `NSTDMutexGuard *guard` - A handle to the mutex's protected data.
+///
+/// # Returns
+///
+/// `NSTDAnyMut data` - A mutable pointer to the mutex's data.
+NSTDAPI NSTDAnyMut nstd_mutex_get_mut(NSTDMutexGuard *guard);
+
+/// The non-blocking variant of `nstd_mutex_lock` returning an uninitialized "none" result if the
+/// mutex is locked by another thread.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutex *mutex` - The mutex to lock.
+///
+/// # Returns
+///
+/// `NSTDOptionalMutexLockResult guard` - A handle to the mutex's protected data.
+NSTDAPI NSTDOptionalMutexLockResult nstd_mutex_try_lock(const NSTDMutex *mutex);
+
+/// Unlocks a mutex by consuming a mutex guard.
+///
+/// # Parameters:
+///
+/// - `NSTDMutexGuard guard` - The mutex guard.
+NSTDAPI void nstd_mutex_unlock(NSTDMutexGuard guard);
+
+/// Frees an instance of `NSTDMutex`.
+///
+/// # Parameters:
+///
+/// - `NSTDMutex mutex` - The mutex to free.
+NSTDAPI void nstd_mutex_free(NSTDMutex mutex);
+
 #endif
--- src/mutex.rs
+++ src/mutex.rs
@@ -1,1 +1,135 @@
 //! A mutual exclusion primitive useful for protecting shared data.
+use crate::{
+    core::{optional::NSTDOptional, result::NSTDResult},
+    heap_ptr::{nstd_heap_ptr_get, nstd_heap_ptr_get_mut, NSTDHeapPtr},
+    NSTDAny, NSTDAnyMut,
+};
+use std::sync::{Mutex, MutexGuard, TryLockError};
+
+/// A mutual exclusion primitive useful for protecting shared data.
+pub type NSTDMutex = Box<Mutex<NSTDHeapPtr>>;
+
+/// A guard providing access to a mutex's protected data.
+pub type NSTDMutexGuard<'a> = Box<MutexGuard<'a, NSTDHeapPtr>>;
+
+/// A lock result returned from `nstd_mutex_lock` containing the mutex guard whether or not the
+/// data is poisoned.
+pub type NSTDMutexLockResult<'a> = NSTDResult<NSTDMutexGuard<'a>, NSTDMutexGuard<'a>>;
+
+/// An optional value of type `NSTDMutexLockResult`.
+///
+/// This type is returned from `nstd_mutex_try_lock` where the uninitialized variant means that the
+/// function would block.
+pub type NSTDOptionalMutexLockResult<'a> = NSTDOptional<NSTDMutexLockResult<'a>>;
+
+/// Creates a new mutual exclusion primitive.
+///
+/// # Parameters:
+///
+/// - `NSTDHeapPtr data` - The data to protect.
+///
+/// # Returns
+///
+/// `NSTDMutex mutex` - The new mutex protecting `data`.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_mutex_new(data: NSTDHeapPtr) -> NSTDMutex {
+    Box::new(Mutex::new(data))
+}
+
+/// Waits for a mutex lock to become acquired, returning a guard wrapping the protected data.
+///
+/// Attempting to call this function on a thread that already owns the lock will either result in a
+/// panic or a deadlock.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutex *mutex` - The mutex to lock.
+///
+/// # Returns
+///
+/// `NSTDMutexLockResult guard` - A handle to the mutex's protected data.
+///
+/// # Panics
+///
+/// This operation may panic if the lock is already held by the current thread.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_mutex_lock(mutex: &NSTDMutex) -> NSTDMutexLockResult {
+    match mutex.lock() {
+        Ok(guard) => NSTDResult::Ok(Box::new(guard)),
+        Err(err) => NSTDResult::Err(Box::new(err.into_inner())),
+    }
+}
+
+/// The non-blocking variant of `nstd_mutex_lock` returning an uninitialized "none" result if the
+/// mutex is locked by another thread.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutex *mutex` - The mutex to lock.
+///
+/// # Returns
+///
+/// `NSTDOptionalMutexLockResult guard` - A handle to the mutex's protected data.
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_mutex_try_lock(mutex: &NSTDMutex) -> NSTDOptionalMutexLockResult {
+    match mutex.try_lock() {
+        Ok(guard) => NSTDOptional::Some(NSTDResult::Ok(Box::new(guard))),
+        Err(err) => match err {
+            TryLockError::WouldBlock => NSTDOptional::None,
+            TryLockError::Poisoned(err) => {
+                NSTDOptional::Some(NSTDResult::Err(Box::new(err.into_inner())))
+            }
+        },
+    }
+}
+
+/// Returns a pointer to a mutex's raw data.
+///
+/// # Parameters:
+///
+/// - `const NSTDMutexGuard *guard` - A handle to the mutex's protected data.
+///
+/// # Returns
+///
+/// `NSTDAny data` - A pointer to the mutex's data.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_mutex_get(guard: &NSTDMutexGuard) -> NSTDAny {
+    nstd_heap_ptr_get(guard)
+}
+
+/// Returns a mutable pointer to a mutex's raw data.
+///
+/// # Parameters:
+///
+/// - `NSTDMutexGuard *guard` - A handle to the mutex's protected data.
+///
+/// # Returns
+///
+/// `NSTDAnyMut data` - A mutable pointer to the mutex's data.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub extern "C" fn nstd_mutex_get_mut(guard: &mut NSTDMutexGuard) -> NSTDAnyMut {
+    nstd_heap_ptr_get_mut(guard)
+}
+
+/// Unlocks a mutex by consuming a mutex guard.
+///
+/// # Parameters:
+///
+/// - `NSTDMutexGuard guard` - The mutex guard.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_mutex_unlock(guard: NSTDMutexGuard) {}
+
+/// Frees an instance of `NSTDMutex`.
+///
+/// # Parameters:
+///
+/// - `NSTDMutex mutex` - The mutex to free.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn nstd_mutex_free(mutex: NSTDMutex) {}
