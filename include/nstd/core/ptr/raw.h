673c49fae1078729be5f62d5d223d04362c73c44
RustEnthusiast <RustEnthusiast@gmail.com>
2023-06-12 21:02:24 -0500

    Updated `core.ptr.raw` docs.

--- include/nstd/core/ptr/raw.h
+++ include/nstd/core/ptr/raw.h
@@ -2,14 +2,16 @@
 #define NSTD_CORE_PTR_RAW_H
 #include "../../nstd.h"
 
-/// Creates a new dangling immutable pointer with valid alignment for any scalar type.
+/// Creates a new dangling pointer to some immutable memory. The pointer is guaranteed to have valid
+/// alignment for any scalar type.
 ///
 /// # Returns
 ///
 /// `NSTDAny dangling` - The new dangling raw pointer.
 NSTDAPI NSTDAny nstd_core_ptr_raw_dangling(void);
 
-/// Creates a new dangling mutable pointer with valid alignment for any scalar type.
+/// Creates a new dangling pointer to some mutable memory. The pointer is guaranteed to have valid
+/// alignment for any scalar type.
 ///
 /// # Returns
 ///
